f19c008b9474839b662bafae8444ec661a8538bd
Alex Resnick <adr8292@gmail.com>
2018-01-06 16:12:52 -0500

    display blank array if no values

--- site/includes/functions/report_functions.php
+++ site/includes/functions/report_functions.php
@@ -5,13 +5,11 @@ function topHourUsers($year) {
 		$year = date('Y');
 	}
 
-	$sel = '';
-	$joins = '';
-	$where = '';
-	$order = '';
+	$data = array();
 	$query = 'SELECT SUM(time_to_sec(timediff(a.time_out, a.time_in)) / 3600) as hours, year(a.time_in) as year, u.* FROM meeting_hours a LEFT JOIN users u USING (user_id) HAVING year = '.db_quote($year).' GROUP BY u.user_id,year ORDER BY year DESC, hours DESC LIMIT 5';
 	$result = db_select($query);
-	return $result;
+	$data = $result ? $result : array();
+	return $data;
 }
 
 
