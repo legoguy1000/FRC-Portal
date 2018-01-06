4e6ed865b20b929dba65d61f0efc726d03b0aeca
Alex Resnick <adr8292@gmail.com>
2018-01-06 15:38:05 -0500

    prevent blank gender or user_type

--- site/reportsActiveUsersPerYear.php
+++ site/reportsActiveUsersPerYear.php
@@ -22,23 +22,23 @@ foreach($series as $se) {
 	$data[$se] = array_fill_keys($years,0);
 }
 //$total = array_fill_keys($years,0);
-$query = 'SELECT COUNT(DISTINCT(m.user_id)) as user_count, YEAR(m.time_in) as year, u.user_type FROM meeting_hours m LEFT JOIN users u USING(user_id) GROUP BY year,u.user_type';
+$query = 'SELECT COUNT(DISTINCT(m.user_id)) as user_count, YEAR(m.time_in) as year, u.user_type FROM meeting_hours m LEFT JOIN users u USING(user_id) WHERE u.user_type <> ""  GROUP BY year,u.user_type';
 $result = db_select($query);
 foreach($result as $re) {
 	$user_type = $re['user_type'];
 	$year = (integer) $re['year'];
 	$uc = (integer) $re['user_count'];
-	
+
 	$data[$user_type.'s'][$year] = $uc;
 	//$total[$year] = array_sum(array_column($data,$year));
 }
-$query = 'SELECT COUNT(DISTINCT(m.user_id)) as user_count, YEAR(m.time_in) as year, u.gender FROM meeting_hours m LEFT JOIN users u USING(user_id) GROUP BY year,u.gender';
+$query = 'SELECT COUNT(DISTINCT(m.user_id)) as user_count, YEAR(m.time_in) as year, u.gender FROM meeting_hours m LEFT JOIN users u USING(user_id) WHERE u.gender <> "" GROUP BY year,u.gender';
 $result = db_select($query);
 foreach($result as $re) {
 	$gender = $re['gender'];
 	$year = (integer) $re['year'];
 	$uc = (integer) $re['user_count'];
-	
+
 	$data[$gender.'s'][$year] = $uc;
 	//$total[$year] = array_sum(array_column($data,$year));
 }
