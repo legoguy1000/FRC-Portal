3c2af89eabbe3626d9c8e35f59e7460cbb4e3cc3
Alex Resnick <adr8292@gmail.com>
2018-01-06 16:07:58 -0500

    add limit of 5 top users

--- site/includes/functions/report_functions.php
+++ site/includes/functions/report_functions.php
@@ -4,11 +4,11 @@ function topHourUsers($year) {
 	if(!isset($year) || $year == '') {
 		$year = date('Y');
 	}
-	
+
 	$sel = 'SUM(time_to_sec(timediff(a.time_out, a.time_in)) / 3600) as hours, year(a.time_in) as year';
 	$joins = 'LEFT JOIN meeting_hours a USING (user_id) GROUP BY user_id,year';
 	$where = 'HAVING year = '.db_quote($year).'';
-	$order = 'ORDER BY year DESC, hours DESC';
+	$order = 'ORDER BY year DESC, hours DESC LIMIT 5';
 	$query = userQuery($sel,$joins, $where, $order);
 
 	/* $query = 'SELECT users.*, CONCAT(users.fname," ",users.lname) AS full_name, schools.school_name, SUM(time_to_sec(timediff(a.time_out, a.time_in)) / 3600) as hours, year(a.time_in) as year
