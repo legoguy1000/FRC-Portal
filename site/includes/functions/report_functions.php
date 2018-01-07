230f0d7d7bc4d7a194bd9f07457bdcb0607962c1
Alex Resnick <adr8292@gmail.com>
2018-01-07 14:44:48 -0500

    fixed query

--- site/includes/functions/report_functions.php
+++ site/includes/functions/report_functions.php
@@ -7,9 +7,9 @@ function topHourUsers($year) {
 
 	$data = array();
 	$sel = 'a.*';
-	$joins = ' LEFT JOIN (SELECT SUM(time_to_sec(timediff(mh.time_out, mh.time_in)) / 3600) as hours, year(mh.time_in) as year FROM meeting_hours mh) a USING (user_id) ';
-	$where = 'WHERE year(a.time_in) = '.db_quote($year).' GROUP BY a.user_id ';
-	$order = 'ORDER BY hours DESC,RAND() LIMIT 5';
+	$joins = ' LEFT JOIN (SELECT mh.*, SUM(time_to_sec(timediff(mh.time_out, mh.time_in)) / 3600) as hours, year(mh.time_in) as year FROM meeting_hours mh WHERE year(mh.time_in) = '.db_quote($year).' GROUP BY mh.user_id ) a USING (user_id) ';
+	$where = '';
+	$order = 'ORDER BY a.hours DESC,a.time_in DESC LIMIT 5';
 	$query = userQuery($sel,$joins, $where, $order);
 	$result = db_select($query);
 	$data = $result ? $result : array();
