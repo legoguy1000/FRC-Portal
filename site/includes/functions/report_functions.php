f41924c15bc5cf5e0e3fc2cfaad9dcbd3fcb2f3d
Alex Resnick <adr8292@gmail.com>
2018-01-07 14:37:14 -0500

    misssing ;

--- site/includes/functions/report_functions.php
+++ site/includes/functions/report_functions.php
@@ -10,7 +10,7 @@ function topHourUsers($year) {
 	$joins = ' LEFT JOIN (SELECT SUM(time_to_sec(timediff(mh.time_out, mh.time_in)) / 3600) as hours, year(mh.time_in) as year FROM meeting_hours mh) a USING (user_id) ';
 	$where = 'WHERE year(a.time_in) = '.db_quote($year).' GROUP BY a.user_id ';
 	$order = 'ORDER BY hours DESC,RAND() LIMIT 5';
-	$query = userQuery($sel,$joins, $where, $order)
+	$query = userQuery($sel,$joins, $where, $order);
 	$result = db_select($query);
 	$data = $result ? $result : array();
 	return $data;
