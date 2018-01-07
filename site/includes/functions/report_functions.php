86788226fc2566429b5209102cec9f8bb5175508
Alex Resnick <adr8292@gmail.com>
2018-01-07 14:53:03 -0500

    by last name

--- site/includes/functions/report_functions.php
+++ site/includes/functions/report_functions.php
@@ -9,7 +9,7 @@ function topHourUsers($year) {
 	$sel = 'a.*';
 	$joins = ' LEFT JOIN (SELECT mh.*, SUM(time_to_sec(timediff(mh.time_out, mh.time_in)) / 3600) as hours, year(mh.time_in) as year FROM meeting_hours mh WHERE year(mh.time_in) = '.db_quote($year).' GROUP BY mh.user_id ) a USING (user_id) ';
 	$where = 'WHERE a.hours > 0';
-	$order = 'ORDER BY a.hours DESC,a.hours_id DESC LIMIT 5';
+	$order = 'ORDER BY a.hours DESC,users.lname ASC LIMIT 5';
 	$query = userQuery($sel,$joins, $where, $order);
 	$result = db_select($query);
 	$data = $result ? $result : array();
