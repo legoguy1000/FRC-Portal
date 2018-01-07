00f18fd0f3180aa2761195a17fb990c2f5c1ef8b
Alex Resnick <adr8292@gmail.com>
2018-01-07 14:26:53 -0500

    fixed WHERE clause for year and modified order by hours then random if equal

--- site/includes/functions/report_functions.php
+++ site/includes/functions/report_functions.php
@@ -6,7 +6,7 @@ function topHourUsers($year) {
 	}
 
 	$data = array();
-	$query = 'SELECT SUM(time_to_sec(timediff(a.time_out, a.time_in)) / 3600) as hours, year(a.time_in) as year, u.* FROM meeting_hours a LEFT JOIN users u USING (user_id) HAVING year = '.db_quote($year).' GROUP BY u.user_id,year ORDER BY year DESC, hours DESC LIMIT 5';
+	$query = 'SELECT SUM(time_to_sec(timediff(a.time_out, a.time_in)) / 3600) as hours, year(a.time_in) as year, u.* FROM meeting_hours a LEFT JOIN users u USING (user_id) WHERE year(a.time_in) = '.db_quote($year).' GROUP BY a.user_id ORDER BY hours DESC,RAND() LIMIT 5';
 	$result = db_select($query);
 	$data = $result ? $result : array();
 	return $data;
