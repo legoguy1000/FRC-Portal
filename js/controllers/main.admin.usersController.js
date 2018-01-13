62dbd5a5e5e1338d3bb7c7478ae4ab1897339770
Alex Resnick <adr8292@gmail.com>
2018-01-13 11:18:23 -0500

    I think I got it

--- js/controllers/main.admin.usersController.js
+++ js/controllers/main.admin.usersController.js
@@ -60,9 +60,7 @@ function mainAdminUsersController($timeout, $q, $scope, $state, $timeout, school
 	vm.selectedItem = null;
 	vm.searchText = null;
 	vm.querySearch = function(search) {
-		usersService.searchQueryBuilder(search).then(function(response){
-			return response;
-	 });
+		return usersService.searchQueryBuilder(search);
 	}
 
 }
