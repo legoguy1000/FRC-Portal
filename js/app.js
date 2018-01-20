7df4d2c393a14ff57a6770873c091971e1205f13
Alex Resnick <adr8292@gmail.com>
2018-01-20 10:10:53 -0500

    one more try

--- js/app.js
+++ js/app.js
@@ -507,5 +507,5 @@ angular.module('FrcPortal', [
 	$authProvider.storageType = 'localStorage';
 })
 .config(['momentPickerProvider', function (momentPickerProvider) {
-	//momentPickerProvider.options({ hoursFormat: 'h:mm a' });
+	momentPickerProvider.options({ hoursFormat: 'LT' });
 }]);
