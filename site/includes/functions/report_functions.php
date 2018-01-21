53fc481a2a9121d9330995c041df10e3745d9f11
Alex Resnick <adr8292@gmail.com>
2018-01-21 10:10:10 -0500

    casting as decimal

--- site/includes/functions/report_functions.php
+++ site/includes/functions/report_functions.php
@@ -7,7 +7,7 @@ function topHourUsers($year) {
 
 	$data = array();
 	$sel = 'a.*';
-	$joins = ' LEFT JOIN (SELECT mh.*, SUM(time_to_sec(timediff(mh.time_out, mh.time_in)) / 3600) as hours, year(mh.time_in) as year FROM meeting_hours mh WHERE year(mh.time_in) = '.db_quote($year).' GROUP BY mh.user_id ) a USING (user_id) ';
+	$joins = ' LEFT JOIN (SELECT mh.*, CAST(SUM(time_to_sec(timediff(mh.time_out, mh.time_in)) / 3600) as decimal(6,2)) as hours, year(mh.time_in) as year FROM meeting_hours mh WHERE year(mh.time_in) = '.db_quote($year).' GROUP BY mh.user_id ) a USING (user_id) ';
 	$where = 'WHERE a.hours > 0';
 	$order = 'ORDER BY a.hours DESC,users.lname ASC LIMIT 5';
 	$query = userQuery($sel,$joins, $where, $order);
