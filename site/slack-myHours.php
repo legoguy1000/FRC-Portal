ecf980e734cd698d401739c737950f7859ec396e
Alex Resnick <adr8292@gmail.com>
2018-01-27 15:34:13 -0500

    trying to match user

--- site/slack-myHours.php
+++ site/slack-myHours.php
@@ -9,10 +9,11 @@ $where = ' WHERE slack_id = '.db_quote($user_id).' OR slack_id = '.db_quote($use
 $query = userQuery($sel='',$joins='', $where, $order = '');
 $result = db_select_single($query);
 if(!is_null($result)) {
+	die('yes');
 	$userInfo = userSeasonInfo($user_id = $result['user_id'], $year = date('Y'));
 	die($userInfo['total']);
 }  else {
-
+	die('no');
 }
 
 
