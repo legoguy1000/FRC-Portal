3c34f5af8adf50a86a5cdefcefb3dd38071cdbc6
Alex Resnick <adr8292@gmail.com>
2018-01-27 11:34:43 -0500

    add slack ID adding status option

--- site/updateUserPersonalInfo.php
+++ site/updateUserPersonalInfo.php
@@ -1,16 +1,21 @@
 <?php
 include('includes.php');
 use \Firebase\JWT\JWT;
-$authToken = checkToken();
+$authToken = checkToken(true,true);
 //die(json_encode($authToken));
-$json = file_get_contents('php://input'); 
+$json = file_get_contents('php://input');
 $formData = json_decode($json,true);
 
 if(!isset($formData['user_id']) || $formData['user_id'] == '')
 {
-	die(json_encode(array('status'=>false, 'type'=>'warning', 'msg'=>'Invlauser_id user user_id.  Reload the page and try again.')));
+	die(json_encode(array('status'=>false, 'type'=>'warning', 'msg'=>'Invalid user_id.  Reload the page and try again.')));
 }
-verifyUser($formData['user_id'], $authToken['data']['user_id'], $die = true);
+$realUser = verifyUser($formData['user_id'], $authToken['data']['user_id'], $die = false);
+$admin = checkAdmin($user_id, $die = false);
+if(!$realUser && !$admin) {
+	header("HTTP/1.1 403 Forbidden");
+	exit;
+}
 if($formData['fname'] == '' || $formData['lname'] == '') {
 	die(json_encode(array('status'=>false, 'type'=>'warning', 'msg'=>'All Fields are Required')));
 }
@@ -22,17 +27,18 @@ $grad_year = !isset($formData['grad_year']) || $formData['grad_year']== null ? '
 $gender = !isset($formData['gender']) || $formData['gender']== null ? 'NULL' : db_quote($formData['gender']);
 $user_type = !isset($formData['user_type']) || $formData['user_type']== null ? 'NULL' : db_quote($formData['user_type']);
 $school_id = !isset($formData['schoolData']['school_id']) || $formData['schoolData']['school_id']== null ? 'NULL' : db_quote($formData['schoolData']['school_id']);
-$query = 'UPDATE users SET  fname='.db_quote($formData['fname']).', 
-							lname='.db_quote($formData['lname']).', 
-							email='.db_quote($formData['email']).', 
-							team_email='.db_quote($formData['team_email']).', 
-							school_id='.$school_id.', 
-							phone='.$phone.', 
-							grad_year='.$grad_year.', 
-							gender='.$gender.', 
+$query = 'UPDATE users SET  fname='.db_quote($formData['fname']).',
+							lname='.db_quote($formData['lname']).',
+							email='.db_quote($formData['email']).',
+							team_email='.db_quote($formData['team_email']).',
+							school_id='.$school_id.',
+							phone='.$phone.',
+							grad_year='.$grad_year.',
+							gender='.$gender.',
 							user_type='.$user_type.',
-							'.($formData['signin_pin']=='' ? 'signin_pin='.db_quote(hash('SHA256', $formData['time_pin'])).',' : '').'
-							first_login="0"
+							'.($formData['signin_pin']=='' && isset($formData['time_pin']) ? 'signin_pin='.db_quote(hash('SHA256', $formData['time_pin'])).',' : '').'
+							'.(isset($formData['status'] && $admin) ? 'status='.db_quote($formData['status']).',' : '').'
+							'.($realUser) ? 'first_login="0"' : '').'
 							WHERE user_id='.db_quote($formData['user_id']);
 //die($query);
 $result = db_query($query);
@@ -41,19 +47,23 @@ if($result) {
 	if($data === false) {
 		die(json_encode(array('status'=>false, 'type'=>'danger', 'msg'=>'Error getting user information')));
 	}
-	$data['login_method'] = $authToken['data']['login_method'];
-	$key = getIniProp('jwt_key');
-	$token = array(
-		"iss" => "https://portal.team2363.org",
-		"iat" => time(),
-		"exp" => time()+60*60,
-		"jti" => bin2hex(random_bytes(10)),
-		'data' => $data
-	);
-	$jwt = JWT::encode($token, $key);
-	die(json_encode(array('status'=>true, 'type'=>'success', 'msg'=>'Personal Information Saved', 'token'=>$jwt)));
+	$return = array('status'=>true, 'type'=>'success', 'msg'=>'Personal Information Saved');
+	if($realUser) {
+		$data['login_method'] = $authToken['data']['login_method'];
+		$key = getIniProp('jwt_key');
+		$token = array(
+			"iss" => "https://portal.team2363.org",
+			"iat" => time(),
+			"exp" => time()+60*60,
+			"jti" => bin2hex(random_bytes(10)),
+			'data' => $data
+		);
+		$jwt = JWT::encode($token, $key);
+		$return['token'] = $jwt;
+	}
+	die(json_encode($return));
 } else {
-	die(json_encode(array('status'=>false, 'type'=>'warning', 'msg'=>'Something went wrong')));	
+	die(json_encode(array('status'=>false, 'type'=>'warning', 'msg'=>'Something went wrong')));
 }
 
 
