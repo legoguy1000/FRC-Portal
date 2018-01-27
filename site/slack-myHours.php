9766bce23c6d833403a40eac9b9544093004b1f1
Alex Resnick <adr8292@gmail.com>
2018-01-27 16:06:32 -0500

    needed space

--- site/slack-myHours.php
+++ site/slack-myHours.php
@@ -6,7 +6,7 @@ $user_id = $_POST['user_id'];
 $user_name = $_POST['user_name'];
 $email = $_POST['text'];
 
-$where = ' WHERE slack_id = '.db_quote($user_id).' OR slack_id = '.db_quote($user_name).($email != '' ? 'OR email = '.db_quote($email).' OR team_email = '.db_quote($email) : '');
+$where = ' WHERE slack_id = '.db_quote($user_id).' OR slack_id = '.db_quote($user_name).($email != '' ? ' OR email = '.db_quote($email).' OR team_email = '.db_quote($email) : '');
 $query = userQuery($sel='',$joins='', $where, $order = '');
 //die($query);
 $result = db_select_single($query);
