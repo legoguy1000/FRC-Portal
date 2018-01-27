7e1b531225f604b707a0f3f58d3541b9514078c6
Alex Resnick <adr8292@gmail.com>
2018-01-27 16:03:44 -0500

    add email param

--- site/slack-myHours.php
+++ site/slack-myHours.php
@@ -4,8 +4,9 @@ include('./includes.php');
 $token = $_POST['token'];
 $user_id = $_POST['user_id'];
 $user_name = $_POST['user_name'];
+$email = $_POST['text'];
 
-$where = ' WHERE slack_id = '.db_quote($user_id).' OR slack_id = '.db_quote($user_name);
+$where = ' WHERE slack_id = '.db_quote($user_id).' OR slack_id = '.db_quote($user_name).' OR email = '.db_quote($email).' OR team_email = '.db_quote($email);
 $query = userQuery($sel='',$joins='', $where, $order = '');
 //die($query);
 $result = db_select_single($query);
@@ -14,7 +15,7 @@ if(!is_null($result)) {
 	$userInfo = userSeasonInfo($user_id = $result['user_id'], $year = date('Y'));
 	die($userInfo[0]['total']);
 }  else {
-	die('no');
+	die('I don\'t know who you are.  Please include send me your email so I can ID you.');
 }
 
 
