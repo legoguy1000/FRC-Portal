cd3d878020fcbd94ec5d35931ab96a57840cf88f
Alex Resnick <adr8292@gmail.com>
2018-01-28 10:40:54 -0500

    convert push notifications to slack

--- site/updateNotificationPreferences.php
+++ site/updateNotificationPreferences.php
@@ -3,7 +3,7 @@ include('includes.php');
 use \Firebase\JWT\JWT;
 $authToken = checkToken(true);
 //die(json_encode($authToken));
-$json = file_get_contents('php://input'); 
+$json = file_get_contents('php://input');
 $formData = json_decode($json,true);
 
 $userId = $authToken['data']['user_id'];
@@ -22,18 +22,18 @@ if($formData['value'] == true) {
 	$query = 'INSERT INTO notification_preferences (pref_id,user_id,method,type) VALUES ('.db_quote($pref_id).','.db_quote($userId).','.db_quote($formData['method']).','.db_quote($formData['type']).')';
 	$result = db_query($query);
 	if($result) {
-		
+
 	} else {
-		die(json_encode(array('status'=>false, 'type'=>'warning', 'msg'=>'Something went wrong')));	
+		die(json_encode(array('status'=>false, 'type'=>'warning', 'msg'=>'Something went wrong')));
 	}
 }
 else if($formData['value'] == false) {
 	$query = 'DELETE FROM notification_preferences WHERE user_id = '.db_quote($userId).' AND method = '.db_quote($formData['method']).' AND type = '.db_quote($formData['type']);
 	$result = db_query($query);
 	if($result) {
-		
+
 	} else {
-		die(json_encode(array('status'=>false, 'type'=>'warning', 'msg'=>'Something went wrong')));	
+		die(json_encode(array('status'=>false, 'type'=>'warning', 'msg'=>'Something went wrong')));
 	}
 }
 
