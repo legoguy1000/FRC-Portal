4462c9c97f16e99d4a2dd8a36be7100f65e21647
Alex Resnick <adr8292@gmail.com>
2018-02-11 20:43:29 -0500

    use NgMap

--- js/controllers/main.event.infoController.js
+++ js/controllers/main.event.infoController.js
@@ -1,10 +1,14 @@
 angular.module('FrcPortal')
-.controller('main.event.infoController', ['$timeout', '$q', '$scope', '$state', 'eventsService', '$mdDialog', '$log','$stateParams','seasonsService',
+.controller('main.event.infoController', ['$timeout', '$q', '$scope', '$state', 'eventsService', '$mdDialog', '$log','$stateParams','seasonsService', 'NgMap',
 	mainAdminEventInfoController
 ]);
-function mainAdminEventInfoController($timeout, $q, $scope, $state, eventsService, $mdDialog, $log,$stateParams,seasonsService) {
+function mainAdminEventInfoController($timeout, $q, $scope, $state, eventsService, $mdDialog, $log,$stateParams,seasonsService,NgMap) {
 	var vmi = this;
-
+	NgMap.getMap().then(function(map) {
+    console.log(map.getCenter());
+    console.log('markers', map.markers);
+    console.log('shapes', map.shapes);
+  });
 
 
 }
