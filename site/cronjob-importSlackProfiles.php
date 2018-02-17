532b75fb062432c20038812d595ee6f76038854b
Alex Resnick <adr8292@gmail.com>
2018-02-17 13:51:30 -0500

    loop through slack users and match emails to set slack id

--- site/cronjob-importSlackProfiles.php
+++ site/cronjob-importSlackProfiles.php
@@ -3,13 +3,27 @@ include('includes.php');
 
 $users = array();
 //$data = http_get('https://slack.com/api/users.list?token=xoxp-10783605152-80482018594-316256639697-04ba2a06532f0849c52bed9783c62fce');
-$url = 'https://slack.com/api/users.list?token=xoxp-10783605152-80482018594-316256639697-04ba2a06532f0849c52bed9783c62fce';
+//$slack_token = getIniProp('slack_token');
+$slack_token = 'xoxp-10783605152-80482018594-316256639697-04ba2a06532f0849c52bed9783c62fce'
+$url = 'https://slack.com/api/users.list?token='.$slack_token;
 $ch = curl_init();
 curl_setopt($ch,CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 $result = curl_exec($ch);
 curl_close($ch);
-$data = json_decode($result);
-echo $result;
+if($result) {
+	$data = json_decode($result);
+	if(isset($data['members']) && is_array($data['members']) && count($data['members']) > 0) {
+		$users = $data['members'];
+		foreach($users as $user) {
+			$user_id = $user['id'];
+			$profile = $user['profile'];
+			if(isset($profile['email']) && $profile['email'] != '') {
+				$email = $profile['email'];
+				$query = 'UPDATE users SET slack_id = '.db_quote($user_id).' WHERE email='.db_quote($email).' OR team_email='.db_quote($email);
+				$result = db_query($query);
+			}
+	}
+}
 ?>
