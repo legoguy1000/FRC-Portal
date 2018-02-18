e2591134beadc5d971ce46d828720329795a38cc
Alex Resnick <adr8292@gmail.com>
2018-02-17 20:56:34 -0500

    use email or f/l name

--- site/cronjob-importSlackProfiles.php
+++ site/cronjob-importSlackProfiles.php
@@ -17,9 +17,22 @@ if($result) {
 		foreach($users as $user) {
 			$user_id = $user['id'];
 			$profile = $user['profile'];
+			$whereArr = array();
 			if(isset($profile['email']) && $profile['email'] != '') {
 				$email = $profile['email'];
-				$query = 'UPDATE users SET slack_id = '.db_quote($user_id).' WHERE email='.db_quote($email).' OR team_email='.db_quote($email);
+				$whereArr[] = '(email='.db_quote($email).' OR team_email='.db_quote($email).')';
+			}
+			if(isset($profile['first_name']) && $profile['first_name'] != '' && isset($profile['last_name']) && $profile['last_name'] != '') {
+				$first_name = $profile['first_name'];
+				$last_name = $profile['last_name'];
+				$whereArr[] = '(fname='.db_quote($first_name).' AND lname='.db_quote($last_name).')';
+			}
+			$where = count($whereArr) > 0 ? 'WHERE '.implode(' OR ', $whereArr) : '';
+			$q = userQuery($sel='',$joins='', $where, $order = '');
+			$result = db_select_single($q);
+			if(!is_null($result)) {
+				$user_id = $result['user_id'];
+				$query = 'UPDATE users SET slack_id = '.db_quote($user_id).' WHERE user_id='.db_quote($user_id);
 				$result = db_query($query);
 			}
 		}
