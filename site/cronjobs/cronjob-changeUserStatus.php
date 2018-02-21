3ccde5e446fb9163e6ac1ff1bb2b161c6b23f27e
Alex Resnick <adr8292@gmail.com>
2018-02-21 18:06:05 -0500

    update htaccess files to allow local access

---
+++ site/cronjobs/cronjob-changeUserStatus.php
@@ -0,0 +1,9 @@
+<?php
+include('../includes.php');
+
+$users = array();
+if(date('n') == 7) {
+	$query = 'UPDATE users SET status="0",admin="0",former_student="1" WHERE user_type="Student" AND grad_year IS NOT NULL AND grad_year=YEAR(CURRENT_DATE) AND MONTH(CURRENT_DATE)=7';
+	$result = db_select($query);
+}
+?>
