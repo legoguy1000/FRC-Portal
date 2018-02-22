57c2aac88b04c3f28c5683d5017e67e0a7dc38fe
Alex Resnick <adr8292@gmail.com>
2018-02-21 21:36:15 -0500

    clear sel and join

--- site/slackAction.php
+++ site/slackAction.php
@@ -4,7 +4,7 @@ include('./includes.php');
 
 
 $where = ' WHERE users.user_uid="5a11bd670484e"';
-$query = userQuery($sel, $joins, $where, $order='');
+$query = userQuery($sel='', $joins='', $where, $order='');
 $result = db_select_single($query);
 emailUser($userData = $result,$subject = 'Slack Button Response',$content = json_encode($_POST),$attachments = false);
 
