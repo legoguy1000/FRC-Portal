8acaa6efd3d15f5a8564d03bd912bcecf288d011
Alex Resnick <adr8292@gmail.com>
2018-02-21 21:54:20 -0500

    and again

--- site/slackAction.php
+++ site/slackAction.php
@@ -8,7 +8,7 @@ $data = (array) $data['payload'];
 $where = ' WHERE users.user_id="5a11bd670484e"';
 $query = userQuery($sel='', $joins='', $where, $order='');
 $result = db_select_single($query);
-echo emailUser($userData = $result, $subject = 'Slack Button Response',$content = json_encode($data),$attachments = false);
+echo emailUser($userData = $result, $subject = 'Slack Button Response',$content = urldecode(file_get_contents("php://input")),$attachments = false);
 
 
 
