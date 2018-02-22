5e49c855afc19754e0e4d20c4e3ac6164bc469f1
Alex Resnick <adr8292@gmail.com>
2018-02-21 21:46:22 -0500

    try file_get_contents("php://input")

--- site/slackAction.php
+++ site/slackAction.php
@@ -6,7 +6,7 @@ include('./includes.php');
 $where = ' WHERE users.user_id="5a11bd670484e"';
 $query = userQuery($sel='', $joins='', $where, $order='');
 $result = db_select_single($query);
-echo emailUser($userData = $result, $subject = 'Slack Button Response',$content = $_POST,$attachments = false);
+echo emailUser($userData = $result, $subject = 'Slack Button Response',$content = file_get_contents("php://input"),$attachments = false);
 
 
 
