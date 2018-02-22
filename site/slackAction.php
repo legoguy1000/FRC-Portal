5da468b1d0e12cfd5f115ac8f1bcce4595000860
Alex Resnick <adr8292@gmail.com>
2018-02-21 21:55:51 -0500

    one more

--- site/slackAction.php
+++ site/slackAction.php
@@ -2,13 +2,13 @@
 include('./includes.php');
 
 $data = array();
-parse_str(urldecode(file_get_contents("php://input")), $data);
-$data = (array) $data['payload'];
+parse_str(file_get_contents("php://input"), $data);
+$data = urldecode($data['payload']);
 
 $where = ' WHERE users.user_id="5a11bd670484e"';
 $query = userQuery($sel='', $joins='', $where, $order='');
 $result = db_select_single($query);
-echo emailUser($userData = $result, $subject = 'Slack Button Response',$content = urldecode(file_get_contents("php://input")),$attachments = false);
+echo emailUser($userData = $result, $subject = 'Slack Button Response',$content = $data,$attachments = false);
 
 
 
