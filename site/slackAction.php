4a4c00c2f4719319901dba59b40cb1f4f919ba72
Alex Resnick <adr8292@gmail.com>
2018-02-21 21:40:56 -0500

    email not working

--- site/slackAction.php
+++ site/slackAction.php
@@ -6,6 +6,7 @@ include('./includes.php');
 $where = ' WHERE users.user_uid="5a11bd670484e"';
 $query = userQuery($sel='', $joins='', $where, $order='');
 $result = db_select_single($query);
+die(json_encode($result));
 echo emailUser($userData = $result, $subject = 'Slack Button Response',$content = json_encode($_POST),$attachments = false);
 
 
