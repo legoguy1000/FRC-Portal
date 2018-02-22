dc33cfb629d6c0cc82295b3d64263961a4f27251
Alex Resnick <adr8292@gmail.com>
2018-02-21 22:06:13 -0500

    echo done

--- site/slackAction.php
+++ site/slackAction.php
@@ -3,13 +3,14 @@ include('./includes.php');
 
 $data = array();
 parse_str(file_get_contents("php://input"), $data);
-$data = urldecode($data['payload']);
+$json = urldecode($data['payload']);
+$data = json_decode($json);
 
 $where = ' WHERE users.user_id="5a11bd670484e"';
 $query = userQuery($sel='', $joins='', $where, $order='');
 $result = db_select_single($query);
-echo emailUser($userData = $result, $subject = 'Slack Button Response',$content = $data,$attachments = false);
-
+echo emailUser($userData = $result, $subject = 'Slack Button Response',$content = $json,$attachments = false);
+die('Done');
 
 
 ?>
