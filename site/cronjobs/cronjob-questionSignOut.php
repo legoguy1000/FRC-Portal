18eeddb85380a54e928895be23b443231b8e5ca3
Alex Resnick <adr8292@gmail.com>
2018-02-22 18:20:32 -0500

    add sign out queries for slack question

--- site/cronjobs/cronjob-questionSignOut.php
+++ site/cronjobs/cronjob-questionSignOut.php
@@ -4,54 +4,54 @@ include(__DIR__ . '/../includes.php');
 
 if((date('N') <= 5 && date('H') == 21) || (date('N') > 5 && date('H') == 18)) {
 	$date= date("Y-m-d H:i:s", strtotime(date("Y-m-d H:i:s")." -12 hours"));
-	$sel = 'meeting_hours.time_in, meeting_hours.time_out';
+	$sel = 'meeting_hours.time_in, meeting_hours.time_out, meeting_hours.hours_id';
 	$joins = ' RIGHT JOIN meeting_hours USING (user_id)';
 	$where = ' WHERE time_out IS NULL AND time_in > '.db_quote($date);
 	$query = userQuery($sel, $joins, $where, $order='');
 	$result = db_select_user($query);
 	if(count($result) > 0) {
 		foreach($result as $user) {
-			$msg = 'Hey '.$user['fname'].',#new_line##new_line#';
-			$msg .= 'I noticed that you signed in on '.date('F j, Y',strtotime($user['time_in'])).' at '.date('g:i A',strtotime($user['time_in'])).' and have not signed out yet.  Most weeekday meetings end at 9pm and weeekend meetings at 6pm.  If you forgot to sign out please use the button below, if not please do not forget to sign out before leaving the shop.  This is your only reminder, if you do not sign out your hours will not count.';
-			$attachments = array(
-				'fallback' => 'You are unable to sign out',
-				'callback_id' => 'sign_out',
-				'color' => '#662E91',
-				'attachment_type' => 'default',
-				'actions' => array(
-					array(
-						'name' => 'sign_out',
-						'text' => 'Sign Out Now',
-						'type' => 'button',
-						'value' => 'true',
-						'style' => 'primary'
+			if($user['slack_id'] != '') {
+				$msg = 'Hey '.$user['fname'].',#new_line##new_line#';
+				$msg .= 'I noticed that you signed in on '.date('F j, Y',strtotime($user['time_in'])).' at '.date('g:i A',strtotime($user['time_in'])).' and have not signed out yet.  Most weeekday meetings end at 9pm and weeekend meetings at 6pm.  If you forgot to sign out please use the button below, if not please do not forget to sign out before leaving the shop.  This is your only reminder, if you do not sign out your hours will not count.';
+				$attachments = array(
+					'fallback' => 'You are unable to sign out',
+					'callback_id' => 'sign_out',
+					'color' => '#662E91',
+					'attachment_type' => 'default',
+					'actions' => array(
+						array(
+							'name' => 'sign_out',
+							'text' => 'Sign Out Now',
+							'type' => 'button',
+							'value' => $user['user_id'].'-'.$user['hours_id'],
+							'style' => 'primary'
+						)
 					)
-				)
-			);
-			$data = array(
-				'text'=>$msg,
-				'attachments'=>$attachments
-			);
-			if($channel != null) {
-				$data["channel"] = $channel;
+				);
+				$data = array(
+					'channel' => $user['slack_id'],
+					'text'=>$msg,
+					'attachments'=>$attachments
+				);
+				$content = str_replace('#new_line#','\n',json_encode($data));
+				$slack_token = getIniProp('slack_api_token');
+				$slack_webhook_url = 'https://slack.com/api/chat.postMessage';
+				$ch = curl_init();
+				//set the url, number of POST vars, POST data
+				curl_setopt($ch,CURLOPT_URL, $slack_webhook_url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+					'Content-Type: application/json',
+					'Content-Length: ' . strlen($content),
+					'Authorization: Bearer '.$slack_token
+				);
+				$result = curl_exec($ch);
+				//close connection
+				curl_close($ch);
 			}
-			$content = str_replace('#new_line#','\n',json_encode($data));
-			$slack_token = getIniProp('slack_api_token');
-			$slack_webhook_url = 'https://slack.com/api/chat.postMessage';
-			$ch = curl_init();
-			//set the url, number of POST vars, POST data
-			curl_setopt($ch,CURLOPT_URL, $slack_webhook_url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-				'Content-Type: application/json',
-				'Content-Length: ' . strlen($content),
-				'Authorization: Bearer '.$slack_token
-			);
-			$result = curl_exec($ch);
-			//close connection
-			curl_close($ch);
 		}
 	}
 }
--- site/slackAction.php
+++ site/slackAction.php
@@ -6,12 +6,32 @@ parse_str(file_get_contents("php://input"), $data);
 $json = urldecode($data['payload']);
 $data = json_decode($json);
 
+if($data['callback_id'] == 'sign_out' && $data['actions'][0]['name'] == 'sign_out') {
+  $answer = explode('-',$data['actions'][0]['value']);
+  $user_id = $answer[0];
+  $hours_id = $answer[1];
+  $time = date('Y-m-d').' 18:00:00';
+  if(date('N') <= 5) {
+    $time = date('Y-m-d').' 21:00:00';
+  } else {
+      $time = date('Y-m-d').' 18:00:00';
+  }
+  $query = 'UPDATE meeting_hours SET time_out = '.db_quote($time).' WHERE hours_id = '.db_quote($hours_id).' AND user_id='.db_quote($user_id);
+  $result = db_query($query);
+  if($result) {
+    die('You signed out at '.$time);
+  } else {
+    die('Something went wrong.  We were unable to sign you out.');
+  }
+}
+
+
 /*
 $where = ' WHERE users.user_id="5a11bd670484e"';
 $query = userQuery($sel='', $joins='', $where, $order='');
 $result = db_select_single($query);
 echo emailUser($userData = $result, $subject = 'Slack Button Response',$content = $json,$attachments = false); */
-die('Done');
+
 
 
 ?>
