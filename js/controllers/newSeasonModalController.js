05f3def4c956ceec2e8702ea6c4fac5c29450400
Alex Resnick <adr8292@gmail.com>
2018-04-22 14:08:12 -0500

    add toast for new season

--- js/controllers/newSeasonModalController.js
+++ js/controllers/newSeasonModalController.js
@@ -8,17 +8,23 @@ function newSeasonModalController($log,$mdDialog,$scope,userInfo,usersService,se
 	vm.cancel = function() {
 		$mdDialog.cancel();
 	}
-	
+
 	vm.data = {};
-	
+
 	vm.addSeason = function() {
 		seasonsService.addSeason(vm.data).then(function(response) {
 			if(response.status) {
 				$mdDialog.hide(response);
 			}
+			$mdToast.show(
+	      $mdToast.simple()
+	        .textContent(response.msg)
+	        .position('top right')
+	        .hideDelay(3000)
+	    );
 		});
 	}
-	
+
 	vm.onYearChange = function (newValue, oldValue) {
 		$log.log('Meeting changed from ' + oldValue + ' to ' + newValue);
 	};
