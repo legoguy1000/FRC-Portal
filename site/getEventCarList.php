4fbbbe04e9c0a5e4823915fd507ee2a1ec5a55a1
Alex Resnick <adr8292@gmail.com>
2018-04-24 17:49:59 -0500

    didn't update api page with new function

--- site/getEventCarList.php
+++ site/getEventCarList.php
@@ -8,7 +8,7 @@ if(isset($_GET['event_id']) && $_GET['event_id'] != '') {
 	$event_id = $_GET['event_id'];
 }
 
-$event = getEventRoomList($event_id);
+$event = getEventCarList($event_id);
 if($event) {
 	die(json_encode(array('status'=>true, 'msg'=>'', 'data'=>$event)));
 } else {
