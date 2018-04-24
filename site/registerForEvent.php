e5b9338bf7ebbf3423db971187f72321f0846bd1
Alex Resnick <adr8292@gmail.com>
2018-04-24 18:31:31 -0500

    add car space to registration

--- site/registerForEvent.php
+++ site/registerForEvent.php
@@ -21,6 +21,27 @@ if(!is_null($result)) {
 	$result1 = db_query($query);
 }
 if($result1) {
+	$user_type = $formData['user_type'];
+	$can_drive = (bool) $formData['can_drive'];
+	if($user_type == 'Mentor' && $can_drive) {
+		$query = 'SELECT * FROM event_cars WHERE event_id='.db_quote($formData['event_id']).' AND user_id='.db_quote($userId);
+		$result = db_select_single($query);
+		if(is_null($result)) {
+			$car_id = uniqid();
+			$query = 'INSERT INTO event_cars (car_id, event_id, user_id, car_space) VALUES ('.db_quote($car_id).', '.db_quote($formData['event_id']).', '.db_quote($userId).', '.db_quote($formData['car_space']).')';
+			$result2 = db_query($query);
+			if($result2) {
+				$query = 'UPDATE event_requirements SET car_id="'.db_quote($car_id).'" WHERE event_id='.db_quote($formData['event_id']).' AND user_id='.db_quote($userId);
+				$result3= db_query($query);
+			} else {
+				$msg = 'Something went wrong';
+				die(json_encode(array('status'=>false, 'type'=>'error', 'msg'=>$msg)));
+			}
+		} else {
+			$msg = 'Something went wrong';
+			die(json_encode(array('status'=>false, 'type'=>'error', 'msg'=>$msg)));
+		}
+	}
 	$msg = 'Registered';
 	die(json_encode(array('status'=>true, 'type'=>'success', 'msg'=>$msg)));
 } else {
