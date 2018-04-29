ed7e59663b6a2a41fe8b5839d9c7cb86322482d2
Alex Resnick <adr8292@gmail.com>
2018-04-29 13:58:29 -0500

    gtg

--- site/getEvent.php
+++ site/getEvent.php
@@ -11,7 +11,7 @@ if(isset($_GET['event_id']) && $_GET['event_id'] != '') {
 if(isset($_GET['reqs']) && ($_GET['reqs'] == 'true' || $_GET['reqs'] == 'false')) {
 	$reqs = $_GET['reqs'];
 }
-var_dump($reqs);
+
 $event = getEvent($event_id, $reqs);
 if($event) {
 	die(json_encode(array('status'=>true, 'msg'=>'', 'data'=>$event)));
