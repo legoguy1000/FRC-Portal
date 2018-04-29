fa635682b790d31d3781014f84c19bad0a3b594f
Alex Resnick <adr8292@gmail.com>
2018-04-29 13:57:29 -0500

    test

--- site/getEvent.php
+++ site/getEvent.php
@@ -8,10 +8,10 @@ $reqs = false;
 if(isset($_GET['event_id']) && $_GET['event_id'] != '') {
 	$event_id = $_GET['event_id'];
 }
-if(isset($_GET['reqs']) && ($_GET['reqs'] == 'true' || $_GET['reqs'] == 'true')) {
+if(isset($_GET['reqs']) && ($_GET['reqs'] == 'true' || $_GET['reqs'] == 'false')) {
 	$reqs = $_GET['reqs'];
 }
-
+die($reqs);
 $event = getEvent($event_id, $reqs);
 if($event) {
 	die(json_encode(array('status'=>true, 'msg'=>'', 'data'=>$event)));
