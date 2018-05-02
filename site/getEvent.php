e829c7f428efd985eeffba26ea9f56f69b827ede
Alex Resnick <adr8292@gmail.com>
2018-05-02 11:44:02 -0500

    not require login for getEvent

--- site/getEvent.php
+++ site/getEvent.php
@@ -1,19 +1,25 @@
 <?php
 include('includes.php');
 
-$authToken = checkToken(true,true);
-$loggedInUser = $authToken['data']['user_id'];
+$authToken = checkToken(false,false);
+$loggedInUser = null;
+$userId = null;
+if($authToken != false) {
+	$loggedInUser = $authToken['data']['user_id'];
+	$userId = $loggedInUser;
+}
+
 
 $event_id = null;
 $reqs = false;
 if(isset($_GET['event_id']) && $_GET['event_id'] != '') {
 	$event_id = $_GET['event_id'];
 }
-if(isset($_GET['reqs']) && ($_GET['reqs'] == 'true' || $_GET['reqs'] == 'false')) {
+if($authToken != false && isset($_GET['reqs']) && ($_GET['reqs'] == 'true' || $_GET['reqs'] == 'false')) {
 	$reqs = $_GET['reqs'];
 }
-$userId = $loggedInUser;
-if(isset($_GET['user_id']) && checkAdmin($loggedInUser, $die = false)) {
+
+if($authToken != false && isset($_GET['user_id']) && checkAdmin($loggedInUser, $die = false)) {
 	$userId = $_GET['user_id'];
 	if($userId == 'null' || $userId == 'all') {
 		$userId = null;
