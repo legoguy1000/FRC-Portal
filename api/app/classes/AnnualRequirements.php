1efe6ee5458d4915cd7676cf99ea721f75b2c426
Alex Resnick <adr8292@gmail.com>
2018-05-02 20:58:05 -0500

    add raw where

--- api/app/classes/AnnualRequirements.php
+++ api/app/classes/AnnualRequirements.php
@@ -78,9 +78,9 @@ class AnnualRequirements extends Eloquent {
             })->where('meeting_hours.time_in', '>=', 'seasons.start_date')
               ->where('meeting_hours.time_in', '<=', 'seasons.bag_day')
               ->whereNull('exempt_hours.exempt_id')
-              ->where('seasons.season_id', '=', '"'.$this->attributes['season_id'].'"')
-              ->where('meeting_hours.user_id', '=', '"'.$this->attributes['user_id'].'"')
-              ->select(DB::raw('SUM(time_to_sec(IFNULL(timediff(meeting_hours.time_out, meeting_hours.time_in),0)) / 3600) as build_season_hours'))->groupBy('meeting_hours.user_i')->get();
+              ->whereRaw('seasons.season_id = ?', $this->attributes['season_id'])
+              ->whereRaw('meeting_hours.user_id = ?', $this->attributes['user_id'])
+              ->select(DB::raw('SUM(time_to_sec(IFNULL(timediff(meeting_hours.time_out, meeting_hours.time_in),0)) / 3600) as build_season_hours'))->groupBy('meeting_hours.user_id')->get();
   }
   public function getOffSeasonHoursAttribute() {
     //SELECT meeting_hours.user_id, year(meeting_hours.time_in), SUM(time_to_sec(IFNULL(timediff(meeting_hours.time_out, meeting_hours.time_in),0)) / 3600) AS off_season_hours, seasons.*
