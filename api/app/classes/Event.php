f4d8e65e8e96cfac71dc20e590b3dfdaa006efe9
Alex Resnick <adr8292@gmail.com>
2018-05-02 19:12:15 -0500

    missing ;

--- api/app/classes/Event.php
+++ api/app/classes/Event.php
@@ -75,7 +75,7 @@ class Event extends Eloquent {
 
     return DB::table('seasons')
             ->join('events', function ($join) {
-                $join->on('events.event_start', '>=', 'seasons.start_date')->on('events.event_end', '<=', 'seasons.end_date')
+                $join->on('events.event_start', '>=', 'seasons.start_date')->on('events.event_end', '<=', 'seasons.end_date');
             })->where('events.event_id', '=', $this->attributes['event_id'])->select('seasons.*')->get();
   }
 
