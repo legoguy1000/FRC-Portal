b278a8b3e721c76f30e51e190ec3c57cecf5fe4e
Alex Resnick <adr8292@gmail.com>
2018-05-05 12:46:40 -0500

    its working

--- api/app/routes/auth.php
+++ api/app/routes/auth.php
@@ -39,7 +39,6 @@ $app->group('/auth', function () {
       }])->where('oauth_id', $id)->where('oauth_provider', $provider)->limit(1)->get();
       if($data->count() > 0) {
         $user = $data[0]->users;
-        die(json_encode($user));
       } else {
         $data = FrcPortal\User::with(['school'])
                 ->where('email', $userData['email'])
