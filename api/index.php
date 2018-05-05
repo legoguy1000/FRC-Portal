c02bfd52fbb5b08126c7c469655740f0d9ad8f72
Alex Resnick <adr8292@gmail.com>
2018-05-04 22:23:16 -0500

    didn't remove extra _test

--- api/index.php
+++ api/index.php
@@ -56,7 +56,7 @@ $container['db'] = function ($container) {
 };*/
 use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
-$capsule->addConnection(array("driver" => "mysql", "host" =>getIniProp('db_host'), "database" => getIniProp('db_name').'_test', "username" => getIniProp('db_user'), "password" => getIniProp('db_pass')));
+$capsule->addConnection(array("driver" => "mysql", "host" =>getIniProp('db_host'), "database" => getIniProp('db_name'), "username" => getIniProp('db_user'), "password" => getIniProp('db_pass')));
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
