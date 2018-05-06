da02c158e5a838e96c694ee272ee915e7db0d1be
Alex Resnick <adr8292@gmail.com>
2018-05-06 11:38:08 -0500

    test

--- api/app/routes/users.php
+++ api/app/routes/users.php
@@ -107,9 +107,7 @@ $app->group('/users', function () {
       $this->get('/{season_id:[a-z0-9]{13}}', function ($request, $response, $args) {
         $user_id = $args['user_id'];
         $season_id = $args['season_id'];
-        $user = FrcPortal\Season::with(['annual_requirements' => function ($query) use ($user_id) {
-                  $query->where('user_id','=',$user_id); // fields from comments table,
-                }])->where('season_id','=',$season_id)->get();
+        $user = FrcPortal\Season::>where('season_id','=',$season_id)->get();
         $responseArr = array('status'=>true, 'msg'=>'', 'data' => $user);
         $response = $response->withJson($responseArr);
         return $response;
