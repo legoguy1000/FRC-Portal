05635333cae1540dabf7476025f20d262424ed93
Alex Resnick <adr8292@gmail.com>
2018-05-07 19:30:50 -0500

    sp error

--- js/controllers/main.admin.eventController.js
+++ js/controllers/main.admin.eventController.js
@@ -55,7 +55,7 @@ function mainAdminEventController($timeout, $q, $scope, $state, eventsService, $
 			vm.users = response.data;
 		});
 	}
-	vm.getUserAnnualRequirements();
+	vm.getUserEventRequirements();
 
 
 
