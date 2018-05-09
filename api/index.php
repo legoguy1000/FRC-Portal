cdcdafff8aed9d1cc6351fc0455400ff5463b60b
Alex Resnick <adr8292@gmail.com>
2018-05-09 12:09:23 -0500

    add each group seperately

--- api/index.php
+++ api/index.php
@@ -28,7 +28,7 @@ $config['db']['prefix'] = '';
 $app = new \Slim\App(['settings' => $config]);
 $app->add(new Tuupola\Middleware\JwtAuthentication([
     "secret" => getIniProp('jwt_key'),
-    "path" => ['/'],
+    "path" => ['/users', '/seasons', '/events', '/reports'],
     "passthrough" => ['/auth'],
 ]));
 $container = $app->getContainer();
