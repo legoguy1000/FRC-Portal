9e9db7ddc6fd8e928cd203edb1c41f315f25c8bb
Alex Resnick <adr8292@gmail.com>
2018-05-10 16:32:19 -0500

    mismatch variable

--- api/app/functions/user_functions.php
+++ api/app/functions/user_functions.php
@@ -1,5 +1,5 @@
 <?php
-function checkAdmin($userId) {
+function checkAdmin($user_id) {
 	$return = false;
 	$user = FrcPortal\User::where('user_id',$user_id)->where('admin',true)->where('status',true)->first();
 	if($user) {
