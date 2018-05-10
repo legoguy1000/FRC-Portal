8a94e43dadedbf96c4e0baadbc6d6884a419166f
Alex Resnick <adr8292@gmail.com>
2018-05-09 22:32:24 -0500

    change to __DIR__

--- site/includes.php
+++ site/includes.php
@@ -3,22 +3,22 @@ ini_set("error_reporting", E_ALL);
 ini_set("expose_php", false);
 date_default_timezone_set('America/New_York');
 
-$root = $_SERVER['DOCUMENT_ROOT'];
+//$root = $_SERVER['DOCUMENT_ROOT'];
 
-require $root.'/site/includes/vendor/autoload.php';
-include($root.'/site/includes/functions/getConfigFile.php');
+require __DIR__.'/includes/vendor/autoload.php';
+include(__DIR__.'/includes/functions/getConfigFile.php');
 
 
 //
-include($root.'/site/includes/functions/db_functions.php');
-include($root.'/site/includes/functions/user_functions.php');
-include($root.'/site/includes/functions/general_functions.php');
-include($root.'/site/includes/functions/school_functions.php');
-include($root.'/site/includes/functions/report_functions.php');
-include($root.'/site/includes/functions/season_functions.php');
-include($root.'/site/includes/functions/event_functions.php');
-include($root.'/site/includes/functions/time_functions.php');
-include($root.'/site/includes/functions/email_functions.php');
+include(__DIR__.'/includes/functions/db_functions.php');
+include(__DIR__.'/includes/functions/user_functions.php');
+include(__DIR__.'/includes/functions/general_functions.php');
+include(__DIR__.'/includes/functions/school_functions.php');
+include(__DIR__.'/includes/functions/report_functions.php');
+include(__DIR__.'/includes/functions/season_functions.php');
+include(__DIR__.'/includes/functions/event_functions.php');
+include(__DIR__.'/includes/functions/time_functions.php');
+include(__DIR__.'/includes/functions/email_functions.php');
 
 
 ?>
