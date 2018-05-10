5f6b8dd6b612abbdeab3c3ef3a790dd369c9685f
Alex Resnick <adr8292@gmail.com>
2018-05-10 16:44:11 -0500

    test

--- api/app/routes/users.php
+++ api/app/routes/users.php
@@ -227,7 +227,7 @@ $app->group('/users', function () {
     });
     $this->put('', function ($request, $response, $args) {
       $authToken = $request->getAttribute("token");
-      $userId = $authToken['data'];
+      $userId = $authToken['data']->user_id;
       $user_id = $args['user_id'];
       $formData = $request->getParsedBody();
       $responseArr = array(
