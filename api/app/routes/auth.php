5f4ccc5f89a50b2634487d2213da0d5cc69cd996
Alex Resnick <adr8292@gmail.com>
2018-05-11 18:38:56 -0500

    test

--- api/app/routes/auth.php
+++ api/app/routes/auth.php
@@ -87,6 +87,7 @@ $app->group('/auth', function () {
     $accessTokenArr = file_get_contents('https://graph.facebook.com/v3.0/oauth/access_token?client_id='.$args['clientId'].'&redirect_uri='.$args['redirectUri'].'&client_secret='.$secret.'&code='.$args['code']);
     //die($accessTokenArr);
     $accessTokenArr = json_decode($accessTokenArr, true);
+    $accessToken = $accessTokenArr['access_token'];
     $fb = new Facebook\Facebook([
       'app_id'  => '1347987445311447',
       'app_secret' => $secret,
