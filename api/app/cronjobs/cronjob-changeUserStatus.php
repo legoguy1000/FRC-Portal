34a5d82672cd590a9d3685ff65b219522d46d0ad
Alex Resnick <adr8292@gmail.com>
2018-05-11 20:06:16 -0500

    finish 1 cronjob

--- api/app/cronjobs/cronjob-changeUserStatus.php
+++ api/app/cronjobs/cronjob-changeUserStatus.php
@@ -3,7 +3,6 @@ include(__DIR__ . '/../includes.php');
 
 $users = array();
 if(date('n') == 7) {
-	$query = 'UPDATE users SET status="0",admin="0",former_student="1" WHERE user_type="Student" AND grad_year IS NOT NULL AND grad_year=YEAR(CURRENT_DATE) AND MONTH(CURRENT_DATE)=7';
-	$result = db_select($query);
+	$users = FrcPortal\User::where('user_type','Student')->whereNotNull('grad_year')->where('grad_year',date('Y'))->update(['status' => false, 'admin' => false, 'former_student' => true]);
 }
 ?>
