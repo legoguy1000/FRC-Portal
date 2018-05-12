2a5e1832e598b42cc1c36011cf9876efcfedd7f3
Alex Resnick <adr8292@gmail.com>
2018-05-11 20:38:22 -0500

    change variable name

--- api/app/cronjobs/cronjob-updateEventsFromGoogle.php
+++ api/app/cronjobs/cronjob-updateEventsFromGoogle.php
@@ -3,7 +3,7 @@ include(__DIR__ . '/../includes.php');
 
 $events = FrcPortal\Event::havingRaw('date(event_start) >= CURDATE()-interval 3 month')->get();
 if(count($events) > 0) {
-	foreach($result as $event) {
+	foreach($events as $event) {
 		if(isset($event->google_cal_id) && $event->google_cal_id != '') {
 			syncGoogleCalendarEvent($event['google_cal_id'], $event['event_id']);
 		}
