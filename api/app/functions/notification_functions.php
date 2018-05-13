545e56e4c37124156abfc46f3cb91b7a1ccb92b6
Alex Resnick <adr8292@gmail.com>
2018-05-13 13:14:03 -0500

    multiple extra ) from cp

--- api/app/functions/notification_functions.php
+++ api/app/functions/notification_functions.php
@@ -120,8 +120,8 @@ function endOfDayHoursToSlack($date = null) {
 	if(!is_null($result)) {
 		$hours = $result->hours;
 		$result = DB::table('meeting_hours')
-							->whereRaw('year(meeting_hours.time_in) = "'.date('Y',strtotime($date)).'"'))
-							->whereRaw('DATE(meeting_hours.time_out)=DATE(meeting_hours.time_in)'))
+							->whereRaw('year(meeting_hours.time_in) = "'.date('Y',strtotime($date)).'"')
+							->whereRaw('DATE(meeting_hours.time_out)=DATE(meeting_hours.time_in)')
 							->select(DB::raw('IFNULL(SUM(time_to_sec(timediff(meeting_hours.time_out, meeting_hours.time_in)) / 3600),0) as hours'))->groupBy(DB::raw('year(a.time_in)'))->first();
 		//$query = 'SELECT IFNULL(SUM(time_to_sec(timediff(a.time_out, a.time_in)) / 3600),0) as hours FROM meeting_hours a WHERE year(a.time_in)='.db_quote(date('Y',strtotime($date))).' GROUP BY year(a.time_in)';
 		//$result = db_select_single($query);
