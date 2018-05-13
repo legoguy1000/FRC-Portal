a699a5132b178ba210cc60dbc8fd503e89bbb5d9
Alex Resnick <adr8292@gmail.com>
2018-05-13 12:08:07 -0500

    typo

--- api/index.php
+++ api/index.php
@@ -55,7 +55,7 @@ $container['db'] = function ($container) {
 };*/
 $app->get('/version', function (Request $request, Response $response, array $args) {
     $responseArr = array(
-      'version': 2.0
+      'version' => 2.0
     );
     $response = $response->withJson($responseArr);
     return $response;
