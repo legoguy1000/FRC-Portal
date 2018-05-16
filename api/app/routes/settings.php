453f5a67a5da0e779db92c465f611dfdc8619f6c
Alex Resnick <adr8292@gmail.com>
2018-05-16 16:46:18 -0500

    add aditional functions and features

--- api/app/routes/settings.php
+++ api/app/routes/settings.php
@@ -39,7 +39,7 @@ $app->group('/settings', function () {
     $response = $response->withJson($responseArr);
     return $response;
   });
-  $this->group('/{setting_id:[a-z0-9]{13}}', function () {
+/*  $this->group('/{setting_id:[a-z0-9]{13}}', function () {
     $this->get('', function ($request, $response, $args) {
       $authToken = $request->getAttribute("token");
       $loggedInUser = $authToken['data']->user_id;
@@ -108,24 +108,96 @@ $app->group('/settings', function () {
       $response = $response->withJson($responseArr);
       return $response;
     });
-  });
-  $this->group('/{setting}', function () {
+  });*/
+  $this->group('/section/{section}', function () {
     $this->get('', function ($request, $response, $args) {
-
-
-      $response = $response->withJson($data);
+      $authToken = $request->getAttribute("token");
+      $loggedInUser = $authToken['data']->user_id;
+      $responseArr = array(
+        'status' => false,
+        'msg' => 'Something went wrong',
+        'data' => null
+      );
+      if(!checkAdmin($loggedInUser)) {
+        $responseArr = array('status'=>false, 'msg'=>'Unauthorized');
+        $response = $response->withJson($responseArr,403);
+        return $response;
+      }
+      $section = $args['section'];
+      $settings = FrcPortal\Setting::where('section',$section);
+      $data = array();
+      foreach($settings as $set) {
+        $temp = $set->value;
+        $type =$set->type;
+        if(strpos('enable', $set->setting) !== false) {
+          $temp = (boolean) $temp;
+        }
+        //settype($temp,$type);
+        $data[$set->setting] = $temp;
+      }
+      $responseArr['status'] = true;
+      $responseArr['msg'] = '';
+      $responseArr['data'] = $data;
+      $response = $response->withJson($responseArr);
       return $response;
     });
     $this->put('', function ($request, $response, $args) {
-
+      $authToken = $request->getAttribute("token");
+      $loggedInUser = $authToken['data']->user_id;
+      $responseArr = array(
+        'status' => false,
+        'msg' => 'Something went wrong',
+        'data' => null
+      );
+      if(!checkAdmin($loggedInUser)) {
+        $responseArr = array('status'=>false, 'msg'=>'Unauthorized');
+        $response = $response->withJson($responseArr,403);
+        return $response;
+      }
+      $section = $args['section'];
+      $formData = $request->getParsedBody();
+      //loop through,
+      //Do update or create
+      foreach($formData as $setting=>$value) {
+        $val = $value;
+        if(strpos('enable', $setting) !== false) {
+          $val = $value != false ? 1:0;
+        }
+        $set = FrcPortal\Setting::updateOrCreate(
+            ['section' => $section, 'setting' => $setting], ['value' => $val]
+        );
+      }
       $response = $response->withJson($responseArr);
       return $response;
     });
-    $this->delete('', function ($request, $response, $args) {
+/*    $this->put('/notification', function ($request, $response, $args) {
+      $authToken = $request->getAttribute("token");
+      $loggedInUser = $authToken['data']->user_id;
+      $responseArr = array(
+        'status' => false,
+        'msg' => 'Something went wrong',
+        'data' => null
+      );
+      if(!checkAdmin($loggedInUser)) {
+        $responseArr = array('status'=>false, 'msg'=>'Unauthorized');
+        $response = $response->withJson($responseArr,403);
+        return $response;
+      }
+      $section = $args['section'];
+      $formData = $request->getParsedBody();
+      $se = array_key_exists('slack_enable',$formData) && $formData['slack_enable'] ? 1:0;
+      $ee = array_key_exists('email_enable',$formData) && $formData['email_enable'] ? 1:0;
+      $set = FrcPortal\Setting::updateOrCreate(
+          ['section' => 'notification', 'setting' => 'slack_enable'], ['value' => $se]
+      );
+      $set = FrcPortal\Setting::updateOrCreate(
+          ['section' => 'notification', 'setting' => 'email_enable'], ['value' => $ee]
+      );
+
 
       $response = $response->withJson($responseArr);
       return $response;
-    });
+    }); */
   });
   $this->post('', function ($request, $response, $args) {
 
