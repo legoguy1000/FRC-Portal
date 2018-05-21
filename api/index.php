6f72f8fab815339f837a4092034b9773a70c3cff
Alex Resnick <adr8292@gmail.com>
2018-05-21 18:47:03 -0500

    bump to 2.4.0

--- api/index.php
+++ api/index.php
@@ -65,7 +65,7 @@ $container['db'] = function ($container) {
 };*/
 $app->get('/version', function (Request $request, Response $response, array $args) {
     $responseArr = array(
-      'version' => '2.3.9'
+      'version' => '2.4.0'
     );
     $response = $response->withJson($responseArr);
     return $response;
