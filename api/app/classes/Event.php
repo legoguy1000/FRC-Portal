23435d18b4aaa77e19eb0aed8e11ef0a75848605
Alex Resnick <adr8292@gmail.com>
2018-05-21 19:14:26 -0500

    fix offeset error

--- api/app/classes/Event.php
+++ api/app/classes/Event.php
@@ -93,7 +93,7 @@ class Event extends Eloquent {
     return DB::table('seasons')
             ->join('events', function ($join) {
                 $join->on('events.event_start', '>=', 'seasons.start_date')->on('events.event_end', '<=', 'seasons.end_date');
-            })->where('events.event_id', '=', $this->attributes['event_id'])->select('seasons.*')->limit(1)->get()[0];
+            })->where('events.event_id', '=', $this->attributes['event_id'])->select('seasons.*')->first();
   }
 
   /**
