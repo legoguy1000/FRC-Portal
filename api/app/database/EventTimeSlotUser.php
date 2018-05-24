26ff4db7fdc32a0f65099ef21ac004bc243f3e68
Alex Resnick <adr8292@gmail.com>
2018-05-24 16:41:19 -0500

    udate table

--- api/app/database/EventTimeSlotUser.php
+++ api/app/database/EventTimeSlotUser.php
@@ -4,11 +4,12 @@ require "../includes.php";
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 Capsule::schema()->create('event_time_slots_users', function ($table) {
-       $table->char('time_slot_id',13)->primary();
-       $table->char('user_id',13)->index();
+  $table->char('time_slot_user_id',13)->primary();
+  $table->char('time_slot_id',13)->index();
+  $table->char('user_id',13)->index();
 
-       $table->foreign('event_id')->references('event_id')->on('events')->onDelete('cascade')->onUpdate('cascade');
-       $table->foreign('user_id')->references('user_id')->on('users')->onDelete('set null')->onUpdate('cascade');
+  $table->foreign('time_slot_id')->references('time_slot_id')->on('event_time_slots')->onDelete('cascade')->onUpdate('cascade');
+  $table->foreign('user_id')->references('user_id')->on('users')->onDelete('set null')->onUpdate('cascade');
 });
 
 ?>
