8801d0afba535f34d203d4e57784c99e102f6f51
Alex Resnick <adr8292@gmail.com>
2018-05-24 16:48:34 -0500

    set nullable

--- api/app/database/EventTimeSlotUser.php
+++ api/app/database/EventTimeSlotUser.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 Capsule::schema()->create('event_time_slots_users', function ($table) {
   $table->char('time_slot_user_id',13)->primary();
   $table->char('time_slot_id',13)->index();
-  $table->char('user_id',13)->index();
+  $table->char('user_id',13)->nullable()->index();
 
   $table->foreign('time_slot_id')->references('time_slot_id')->on('event_time_slots')->onDelete('cascade')->onUpdate('cascade');
   $table->foreign('user_id')->references('user_id')->on('users')->onDelete('set null')->onUpdate('cascade');
