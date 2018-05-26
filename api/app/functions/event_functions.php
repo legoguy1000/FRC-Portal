1504a741e4218a9f79c77824ceadcca289eefe6c
Alex Resnick <adr8292@gmail.com>
2018-05-25 21:48:21 -0500

    add sync for registration_deadline

--- api/app/functions/event_functions.php
+++ api/app/functions/event_functions.php
@@ -11,43 +11,77 @@ function syncGoogleCalendarEvent($event_id) {
 	if(!is_null($event)) {
 		$google_cal_id = $event->google_cal_id;
 		if(isset($google_cal_id) && $google_cal_id != '') {
-			try {
-				$client = new Google_Client();
-				$client->setDeveloperKey($api_key);
-				$service = new Google_Service_Calendar($client);
-				$gevent = $service->events->get($calendar, $google_cal_id);
-
-				$event->name = $gevent->summary;
-				$event->details = $gevent->description;
-				$event->location = $gevent->location;
-				if(empty($gevent->start->dateTime)) {
-					$event->event_start = $gevent->start->date.' 00:00:00';
-					$event->event_end = $gevent->end->date.' 23:59:59';
-				} else {
-					$event->event_start = date('Y-m-d H:i:s', strtotime($gevent->start->dateTime));
-					$event->event_end =date('Y-m-d H:i:s', strtotime($gevent->end->dateTime));
-				}
-				if($event->save()) {
-					$result['status'] = true;
-					$result['msg'] = $event->name.' synced with Google Calendar';
-					$result['data'] = $event;
-				} else {
-					$result['msg'] = 'Something went wrong updating the event';
-				}
-
-			} catch (Exception $e) {
-					$error = json_decode($e->getMessage(), true);
-	        if($error['error']['code'] == 404) {
-						$result['msg'] = 'Google Calendar event not found';
-					} else {
-						$result['msg'] = 'Something went wrong searching Google Calendar';
+			$gevent = $service->events->get($calendar, $google_cal_id);
+			if($gevent['status'] != false) {
+				$ge = $gevent['data'];
+				$event->name = $ge['summary'];
+				$event->details = $ge['description'];
+				$event->location = $ge['location'];
+				$event->event_start = $ge['event_start'];
+				$event->event_end = $ge['event_end'];
+				if(!is_null($event->registration_deadline_gcalid) && $event->registration_deadline_gcalid != '') {
+					$gevent = $service->events->get($calendar, $event->registration_deadline_gcalid);
+					if($gevent['status'] != false) {
+						$ge = $gevent['data'];
+					 	$event->registration_deadline = $ge['event_end'];
 					}
-	    }
+				}
+			}
+			if($event->save()) {
+				$result['status'] = true;
+				$result['msg'] = $event->name.' synced with Google Calendar';
+				$result['data'] = $event;
+			} else {
+				$result['msg'] = 'Something went wrong updating the event';
+			}
 		} else {
 			$result['msg'] = 'Google Calendar Event ID not found';
 		}
 	} else {
 		$result['msg'] = 'Event ID not found';
+	}
+	return $result;
+}
+
+function getGoogleCalendarEvent($google_cal_id) {
+	$calendar = getSettingsProp('google_calendar_id');
+	$api_key = getSettingsProp('google_api_key');
+	$result = array(
+		'status' => false,
+		'msg' => '',
+		'data' => null
+	);
+	if(isset($google_cal_id) && $google_cal_id != '') {
+		try {
+			$client = new Google_Client();
+			$client->setDeveloperKey($api_key);
+			$service = new Google_Service_Calendar($client);
+			$gevent = $service->events->get($calendar, $google_cal_id);
+			$event = array();
+			$event['name'] = $gevent->summary;
+			$event['details'] = $gevent->description;
+			$event['location'] = $gevent->location;
+			if(empty($gevent->start->dateTime)) {
+				$event['event_start'] = $gevent->start->date.' 00:00:00';
+				$ed = new DateTime($gevent->end->date);
+				$ed->modify("-1 day");
+				$event['event_end'] = $ed->format("Y-m-d").' 23:59:59';
+			} else {
+				$event['event_start'] = date('Y-m-d H:i:s', strtotime($gevent->start->dateTime));
+				$event['event_end'] =date('Y-m-d H:i:s', strtotime($gevent->end->dateTime));
+			}
+			$result['status'] = true;
+			$result['data'] = $event;
+		} catch (Exception $e) {
+				$error = json_decode($e->getMessage(), true);
+        if($error['error']['code'] == 404) {
+					$result['msg'] = 'Google Calendar event not found';
+				} else {
+					$result['msg'] = 'Something went wrong searching Google Calendar';
+				}
+    }
+	} else {
+		$result['msg'] = 'Google Calendar Event ID not found';
 	}
 	return $result;
 }
