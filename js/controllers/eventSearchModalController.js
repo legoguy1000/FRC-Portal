e6bce3c3e16704a04749149913c0bc38a9dbc237
Alex Resnick <adr8292@gmail.com>
2018-05-25 20:09:51 -0500

    seperate google event search from new event

---
+++ js/controllers/eventSearchModalController.js
@@ -0,0 +1,50 @@
+angular.module('FrcPortal')
+.controller('eventSearchModalController', ['$log','$element','$mdDialog', '$scope', 'userInfo', 'usersService', 'schoolsService', 'seasonsService','$mdToast',
+	eventSearchModalController
+]);
+function eventSearchModalController($log,$element,$mdDialog,$scope,userInfo,usersService,eventsService,seasonsService,$mdToast) {
+	var vm = this;
+
+	vm.cancel = function() {
+		$mdDialog.cancel();
+	}
+	vm.data = {};
+	vm.loading = {
+		searchGoogle: false,
+	}
+	vm.searchGoogle = {
+		q:null,
+		timeMax:null,
+		timeMin:null,
+	};
+	vm.googleEvents = {
+		data: [],
+		total: 0
+	};
+	vm.query = {
+		filter: '',
+		limit: 5,
+		order: 'event_start',
+		page: 1
+	};
+
+	vm.searchGoogleFunc = function() {
+		var data = vm.searchGoogle;
+		vm.loading.searchGoogle = eventsService.getGoogleCalendarEvents(data.q, vm.searchGoogle.timeMin, vm.searchGoogle.timeMax).then(function(response) {
+			if(response.status) {
+					vm.googleEvents.data = response.data.results;
+					vm.googleEvents.total = response.data.count;
+			}
+		});
+	}
+
+	vm.selectGoogleEvent = function(data) {
+		vm.data = data;
+		vm.data.start_moment = moment(vm.data.event_start);
+		vm.data.end_moment = moment(vm.data.event_end);
+		vm.showGoogle = false;
+		if(response.status) {
+			$mdDialog.hide(vm.data);
+		}
+	}
+}
--- js/controllers/newEventModalController.js
+++ js/controllers/newEventModalController.js
@@ -1,33 +1,18 @@
 angular.module('FrcPortal')
-.controller('newEventModalController', ['$log','$element','$mdDialog', '$scope', 'userInfo', 'usersService', 'schoolsService', 'seasonsService','$mdToast',
+.controller('newEventModalController', ['$log','$element','$mdDialog', '$scope', 'userInfo', 'usersService', 'schoolsService', 'seasonsService','$mdToast','eventData',
 	newEventModalController
 ]);
-function newEventModalController($log,$element,$mdDialog,$scope,userInfo,usersService,eventsService,seasonsService,$mdToast) {
+function newEventModalController($log,$element,$mdDialog,$scope,userInfo,usersService,eventsService,seasonsService,$mdToast,eventData) {
 	var vm = this;
 
 	vm.cancel = function() {
 		$mdDialog.cancel();
 	}
-	vm.data = {};
-	vm.showGoogle = true;
+	vm.data = eventData;
+
 	vm.loading = {
 		searchGoogle: false,
 	}
-	vm.searchGoogle = {
-		q:null,
-		timeMax:null,
-		timeMin:null,
-	};
-	vm.googleEvents = {
-		data: [],
-		total: 0
-	};
-	vm.query = {
-		filter: '',
-		limit: 5,
-		order: 'event_start',
-		page: 1
-	};
 
 	vm.eventTypes = [
 		'Demo',
@@ -37,30 +22,10 @@ function newEventModalController($log,$element,$mdDialog,$scope,userInfo,usersSe
 		'Other'
 	];
 
-	vm.searchGoogleFunc = function() {
-		var data = vm.searchGoogle;
-		vm.loading.searchGoogle = eventsService.getGoogleCalendarEvents(data.q, vm.searchGoogle.timeMin, vm.searchGoogle.timeMax).then(function(response) {
-			if(response.status) {
-					vm.googleEvents.data = response.data.results;
-					vm.googleEvents.total = response.data.count;
-			}
-		});
-	}
-
-	vm.selectGoogleEvent = function(data) {
-		vm.data = data;
-		vm.data.start_moment = moment(vm.data.event_start);
-		vm.data.end_moment = moment(vm.data.event_end);
-		vm.showGoogle = false;
-
-	}
 
 	vm.backToSearch = function() {
-		vm.data = {};
-		vm.showGoogle = true;
+		$mdDialog.cancel();
 	}
-
-
 
 	vm.addEvent = function() {
 		eventsService.addEvent(vm.data).then(function(response) {
