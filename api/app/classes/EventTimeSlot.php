3bb78c3b91e766ebdd9698205a0a67e189808501
Alex Resnick <adr8292@gmail.com>
2018-05-27 19:24:48 -0500

    back to many many

--- api/app/classes/EventTimeSlot.php
+++ api/app/classes/EventTimeSlot.php
@@ -62,8 +62,8 @@ class EventTimeSlot extends Eloquent {
    * Get the room.
    */
   public function users() {
-      //return $this->belongsToMany('FrcPortal\User', 'event_time_slots_users', 'time_slot_id', 'user_id');
-      return $this->hasManyThrough('FrcPortal\User', 'FrcPortal\EventRequirement', 'ereq_id', 'user_id', 'ereq_id', 'user_id');
+      return $this->belongsToMany('FrcPortal\EventRequirement', 'event_time_slots_users', 'time_slot_id', 'user_id');
+      //return $this->hasManyThrough('FrcPortal\User', 'FrcPortal\EventRequirement', 'ereq_id', 'user_id', 'ereq_id', 'user_id');
   }
 
   public function getTimeStartUnixAttribute() {
