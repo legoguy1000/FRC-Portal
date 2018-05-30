07ba5f4f2a12b535b4650bb540f706accce1e88b
Alex Resnick <adr8292@gmail.com>
2018-05-29 19:30:00 -0500

    nope like this

--- api/app/includes.php
+++ api/app/includes.php
@@ -7,11 +7,6 @@ ini_set("expose_php", false);
 $root = __DIR__;
 require $root.'/vendor/autoload.php';
 include($root.'/functions/getConfigFile.php');
-include($root.'/functions/season_functions.php');
-include($root.'/functions/event_functions.php');
-include($root.'/functions/user_functions.php');
-include($root.'/functions/notification_functions.php');
-
 
 $capsule = new Capsule;
 $capsule->addConnection(array("driver" => "mysql", "host" =>getIniProp('db_host'), "database" => getIniProp('db_name'), "username" => getIniProp('db_user'), "password" => getIniProp('db_pass')));
@@ -21,4 +16,14 @@ $capsule->bootEloquent();
 $tz = getSettingsProp('timezone');
 $time_zone = !is_null($tz) ? $tz:date_default_timezone_get();
 date_default_timezone_set($time_zone);
+
+
+
+include($root.'/functions/season_functions.php');
+include($root.'/functions/event_functions.php');
+include($root.'/functions/user_functions.php');
+include($root.'/functions/notification_functions.php');
+
+
+
 ?>
