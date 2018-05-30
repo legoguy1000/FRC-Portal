af95ccadc2d8a5c029bde9147c271a41af40e7e1
Alex Resnick <adr8292@gmail.com>
2018-05-29 19:29:27 -0500

    set tz at bottom

--- api/app/includes.php
+++ api/app/includes.php
@@ -3,14 +3,10 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 ini_set("error_reporting", E_ALL);
 ini_set("expose_php", false);
 
+
 $root = __DIR__;
 require $root.'/vendor/autoload.php';
 include($root.'/functions/getConfigFile.php');
-$tz = getSettingsProp('timezone');
-$time_zone = !is_null($tz) ? $tz:date_default_timezone_get();
-date_default_timezone_set($time_zone);
-
-
 include($root.'/functions/season_functions.php');
 include($root.'/functions/event_functions.php');
 include($root.'/functions/user_functions.php');
@@ -22,4 +18,7 @@ $capsule->addConnection(array("driver" => "mysql", "host" =>getIniProp('db_host'
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
+$tz = getSettingsProp('timezone');
+$time_zone = !is_null($tz) ? $tz:date_default_timezone_get();
+date_default_timezone_set($time_zone);
 ?>
