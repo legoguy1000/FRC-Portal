1b66de44e9a3bc2b35975efe382e248f3cfc519e
Alex Resnick <adr8292@gmail.com>
2018-06-08 11:31:16 -0500

    index type

--- api/app/database/EventType.php
+++ api/app/database/EventType.php
@@ -5,7 +5,7 @@ require_once "../includes.php";
 use Illuminate\Database\Capsule\Manager as Capsule;
 Capsule::schema()->create('event_types', function ($table) {
        $table->char('type_id',13)->primary();
-       $table->string('type');
+       $table->string('type')->index();
        $table->text('description');
        $table->timestamps();
    });
