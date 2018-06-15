ea4c6f0299fb1551a2b541fcdc6fa2f24c1167c3
Alex Resnick <adr8292@gmail.com>
2018-06-15 16:27:28 -0500

    typo

--- api/app/initialInstall.php
+++ api/app/initialInstall.php
@@ -5,7 +5,7 @@ $version = VERSION;
 
 shell_exec("composer install");
 shell_exec("composer dump-autoload");
-shell_exec("database/_CreateDatabasse.php");
+shell_exec("database/_CreateDatabase.php");
 //shell_exec("mkdir secured");
 //shell_exec("cp config.example.ini secured/config.ini");
 
