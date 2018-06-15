d820e0b5b2683af01eef97e3da72b3f7db8310ac
Alex Resnick <adr8292@gmail.com>
2018-06-15 16:30:36 -0500

    include php not execute

--- api/app/initialInstall.php
+++ api/app/initialInstall.php
@@ -5,7 +5,8 @@ $version = VERSION;
 
 shell_exec("composer install");
 shell_exec("composer dump-autoload");
-shell_exec("database/_CreateDatabase.php");
+//shell_exec("php database/_CreateDatabase.php");
+include_once('database/_CreateDatabase.php');
 //shell_exec("mkdir secured");
 //shell_exec("cp config.example.ini secured/config.ini");
 
