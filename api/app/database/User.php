c3bad7f4a00b09e96b33a1e924afe196a1a37a41
Alex Resnick <adr8292@gmail.com>
2018-06-15 20:03:12 -0500

    create Admin Account

--- api/app/database/User.php
+++ api/app/database/User.php
@@ -26,6 +26,18 @@ Capsule::schema()->create('users', function ($table) {
 
   $table->foreign('school_id')->references('school_id')->on('schools')->onDelete('set null')->onUpdate('cascade');
 });
-
-$user = FrcPortal\User::updateOrCreate(['type' => 'Demo'], ['description' => 'Demo Events']);
+//create Admin Account
+$email = 'admin@example.org';
+$password = bin2hex(openssl_random_pseudo_bytes(4));
+$user = FrcPortal\User::create([
+  'fname' => 'admin',
+  'lname' => 'admin',
+  'email' => $email,
+  'password' => hash('sha512',$password),
+  'user_type' => 'Mentor',
+  'admin' => true,
+]);
+echo 'Admin Account Created:\n';
+echo 'Email: '.$email.'\n';
+echo 'Password: '.$password.'\n\n';
 ?>
