5fa124877ca316b4bb8cad3f24ed436e108a3d24
Alex Resnick <adr8292@gmail.com>
2018-06-18 16:31:51 -0500

    add foreign key if table exists

--- api/app/database/EventRequirement.php
+++ api/app/database/EventRequirement.php
@@ -4,25 +4,30 @@ include_once(__DIR__.'/../includes.php');
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 Capsule::schema()->create('event_requirements', function ($table) {
-       $table->char('ereq_id',13)->primary();
-       $table->char('event_id',13)->index();
-       $table->char('user_id',13)->index();
-       $table->boolean('registration')->default(0);
-       $table->boolean('payment')->default(0);
-       $table->boolean('permission_slip')->default(0);
-       $table->boolean('food')->default(0);
-       $table->char('room_id',13)->nullable()->default(null);
-       $table->boolean('can_drive')->default(0);
-       $table->char('car_id',13)->nullable()->default(null);
-       $table->text('comments');
-       $table->boolean('attendance_confirmed')->default(0);
-       $table->timestamps();
+  $table->char('ereq_id',13)->primary();
+  $table->char('event_id',13)->index();
+  $table->char('user_id',13)->index();
+  $table->boolean('registration')->default(0);
+  $table->boolean('payment')->default(0);
+  $table->boolean('permission_slip')->default(0);
+  $table->boolean('food')->default(0);
+  $table->char('room_id',13)->nullable()->default(null);
+  $table->boolean('can_drive')->default(0);
+  $table->char('car_id',13)->nullable()->default(null);
+  $table->text('comments');
+  $table->boolean('attendance_confirmed')->default(0);
+  $table->timestamps();
 
-       $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade')->onUpdate('cascade');
-       $table->foreign('event_id')->references('event_id')->on('events')->onDelete('cascade')->onUpdate('cascade');
-       $table->foreign('room_id')->references('room_id')->on('event_rooms')->onDelete('set null')->onUpdate('cascade');
-       $table->foreign('car_id')->references('car_id')->on('event_cars')->onDelete('set null')->onUpdate('cascade');
-       $table->unique(['user_id','event_id']);
-   });
-
+  $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+  $table->foreign('event_id')->references('event_id')->on('events')->onDelete('cascade')->onUpdate('cascade');
+  $table->foreign('room_id')->references('room_id')->on('event_rooms')->onDelete('set null')->onUpdate('cascade');
+  $table->foreign('car_id')->references('car_id')->on('event_cars')->onDelete('set null')->onUpdate('cascade');
+  $table->unique(['user_id','event_id']);
+});
+$event_cars = Capsule::schema()->hasTable('event_cars');
+if($event_cars) {
+  Capsule::schema()->table('event_cars', function ($table) {
+    $table->foreign(['user_id','event_id'])->references(['user_id','event_id'])->on('event_requirements')->onDelete('cascade')->onUpdate('cascade');
+  });
+}
 ?>
