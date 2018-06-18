dd636d5e2d60a6710c3cdb3eac99187fc00834fc
Alex Resnick <adr8292@gmail.com>
2018-06-18 17:29:59 -0500

    decalring foreign key twice

--- api/app/database/EventTimeSlotUser.php
+++ api/app/database/EventTimeSlotUser.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 Capsule::schema()->create('event_time_slots_event_requirements', function ($table) {
   $table->char('time_slot_id',13)->index();
   $table->char('ereq_id',13)->nullable()->index();
-
-  $table->foreign('time_slot_id')->references('time_slot_id')->on('event_time_slots')->onDelete('cascade')->onUpdate('cascade');
-  $table->foreign('ereq_id')->references('ereq_id')->on('event_requirements')->onDelete('cascade')->onUpdate('cascade');
 });
 
 ?>
