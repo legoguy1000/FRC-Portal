bbab2750370724a4de725c1feba767c6a93f00fb
Alex Resnick <adr8292@gmail.com>
2018-06-21 17:23:06 -0500

    typo

--- api/app/functions/general_functions.php
+++ api/app/functions/general_functions.php
@@ -286,7 +286,7 @@ function exportDB() {
 	$mysqlHostName = getIniProp('db_host');
 	$mysqlExportPath = $folder.date('Y-m-d H:i:s').' '.$mysqlDatabaseName.'.sql';
 	$worked = null;
-	$command='mysqldump --opt -h '.$mysqlHostName.' -u'.$mysqlUserName.' -p'.$mysqlPassword.' '.$mysqlDatabaseName.' > "' .$mysqlExportPath.'"';
+	$command='mysqldump --opt -h '.$mysqlHostName.' -u '.$mysqlUserName.' -p'.$mysqlPassword.' '.$mysqlDatabaseName.' > "' .$mysqlExportPath.'"';
 	exec($command,$output,$worked);
 	switch($worked){
 	case 0:
