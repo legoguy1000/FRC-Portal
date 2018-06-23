833e5828cc2fb5cd673bbfaaa97bdf463205594b
Alex Resnick <adr8292@gmail.com>
2018-06-23 14:10:31 -0500

    test

--- js/app.js
+++ js/app.js
@@ -482,13 +482,13 @@ angular.module('FrcPortal', [
 	  'contrastLightColors': []
 	});
 	$mdThemingProvider.theme('default')
-		.primaryPalette('tripplehelixpurple', {
+		.primaryPalette('primary', {
       'default': '500', // by default use shade 400 from the pink palette for primary intentions
       'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
       'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
       'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
     })
-		.accentPalette('tripplehelixyellow', {
+		.accentPalette('secondary', {
       'default': '500', // by default use shade 400 from the pink palette for primary intentions
       'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
       'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
