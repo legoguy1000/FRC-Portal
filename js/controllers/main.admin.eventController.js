25be3b3a8826ed536b52f8eb74e2b0e74797177d
Alex Resnick <adr8292@gmail.com>
2018-06-25 18:43:05 -0500

    log response

--- js/controllers/main.admin.eventController.js
+++ js/controllers/main.admin.eventController.js
@@ -377,6 +377,7 @@ function mainAdminEventController($timeout, $q, $scope, $state, eventsService, $
 			}
 		})
 		.then(function(response) {
+			console.log(response);
 			vm.event.registration_deadline_formatted = response.end.long_date;
 			vm.event.registration_deadline_gcalid = response.google_cal_id;
 			$log.info('asdf');
