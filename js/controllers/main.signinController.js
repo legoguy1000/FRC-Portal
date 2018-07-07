b81ee71e44b98eae80a5ba9a41a1591d94eb33c4
Alex Resnick <adr8292@gmail.com>
2018-07-06 19:11:49 -0500

    update signin width and remove keypress watch

--- js/controllers/main.signinController.js
+++ js/controllers/main.signinController.js
@@ -115,47 +115,19 @@ function mainSigninController($rootScope, $timeout, $q, $auth, $scope, signinSer
 
 	vm.keyDown = function(e) {
 		if(e.keyCode == 46 || e.keyCode == 8) {
-        console.log('backspace');
+        //console.log('backspace');
     }
-		console.log(e.keyCode);
+		//console.log(e.keyCode);
 	}
 	$(document).keyup(function (e) {
-	    console.log(e);
+	    //console.log(e);
 			if(vm.pin.length >= 4 && vm.pin.length <= 8) {
 				if(e.originalEvent.code == 'Enter') {
-					e.preventDefault();
-					e.stopPropagation()
-					vm.signinOut();
+					//e.preventDefault();
+				//	e.stopPropagation()
+					//vm.signinOut();
 				}
 			}
-			/*if(vm.pin.length < 8) {
-				if(e.originalEvent.code == 'Digit1' || e.originalEvent.code == 'Numpad1') {
-					vm.pin = vm.pin+'1';
-				} else if(e.originalEvent.code == 'Digit2' || e.originalEvent.code == 'Numpad2') {
-					vm.pin = vm.pin+'2';
-				} else if(e.originalEvent.code == 'Digit3' || e.originalEvent.code == 'Numpad3') {
-					vm.pin = vm.pin+'3';
-				} else if(e.originalEvent.code == 'Digit4' || e.originalEvent.code == 'Numpad4') {
-					vm.pin = vm.pin+'4';
-				} else if(e.originalEvent.code == 'Digit5' || e.originalEvent.code == 'Numpad5') {
-					vm.pin = vm.pin+'5';
-				} else if(e.originalEvent.code == 'Digit6' || e.originalEvent.code == 'Numpad6') {
-					vm.pin = vm.pin+'6';
-				} else if(e.originalEvent.code == 'Digit7' || e.originalEvent.code == 'Numpad7') {
-					vm.pin = vm.pin+'7';
-				} else if(e.originalEvent.code == 'Digit8' || e.originalEvent.code == 'Numpad8') {
-					vm.pin = vm.pin+'8';
-				} else if(e.originalEvent.code == 'Digit9' || e.originalEvent.code == 'Numpad9') {
-					vm.pin = vm.pin+'9';
-				} else if(e.originalEvent.code == 'Digit0' || e.originalEvent.code == 'Numpad0') {
-					vm.pin = vm.pin+'0';
-				}
-			}
-			if(vm.pin.length > 0) {
-					if(e.originalEvent.code == 'Backspace' || e.originalEvent.code == 'Delete') {
-						vm.pin = vm.pin.slice(0, -1);
-					}
-			}*/
 	});
 
 }
