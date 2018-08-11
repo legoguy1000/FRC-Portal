db93227257d26bfb541aff0cc1e288098a491a68
Alex Resnick <adr8292@gmail.com>
2018-08-11 15:20:47 -0400

    test

--- js/controllers/main.eventController.js
+++ js/controllers/main.eventController.js
@@ -13,5 +13,48 @@ function mainEventController($timeout, $q, $scope, $state, eventsService, $mdDia
 		vm.slack_team_id = configItems.slack_team_id;
 
 		vm.event_id = $stateParams.event_id;
-		
+		vm.event = {};
+		vm.getEvent = function () {
+			var user_id = $scope.main.isAuthed ? $scope.main.userInfo.user_id : null;
+			var reqs = $scope.main.isAuthed ? true : false;
+			vm.promise = eventsService.getEvent(vm.event_id, reqs, user_id).then(function(response){
+				vm.event = response.data;
+				$scope.main.title += ' - '+vm.event.name;
+			}
+
+		};
+
+		vm.getEvent();
+		vm.limitOptions = [5,10,25,50,100];
+		vm.query = {
+			filter: '',
+			limit: 5,
+			order: 'full_name',
+			page: 1
+		};
+
+
+
+		vm.showRegistrationForm = function(ev) {
+			var eventInfo = vm.event;
+			if(!$scope.main.isAuthed) {
+				return;
+			}
+			$mdDialog.show({
+				controller: eventRegistrationController,
+				controllerAs: 'vm',
+				templateUrl: 'views/partials/eventRegistrationModal.tmpl.html',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+				clickOutsideToClose:true,
+				fullscreen: true, // Only for -xs, -sm breakpoints.
+				locals: {
+					'eventInfo': eventInfo,
+					'userInfo': $scope.main.userInfo
+				}
+			})
+			.then(function(answer) {
+
+			}, function() { });
+		}
 }
