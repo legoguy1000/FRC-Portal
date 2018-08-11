026654363ba36e3017cd31a8d781b11dddbbecd0
Alex Resnick <adr8292@gmail.com>
2018-08-11 15:16:55 -0400

    add google maps embed

--- js/controllers/main.eventController.js
+++ js/controllers/main.eventController.js
@@ -20,7 +20,8 @@ function mainEventController($timeout, $q, $scope, $state, eventsService, $mdDia
 			vm.promise = eventsService.getEvent(vm.event_id, reqs, user_id).then(function(response){
 				vm.event = response.data;
 				$scope.main.title += ' - '+vm.event.name;
-			});
+
+
 		};
 
 		vm.getEvent();
@@ -30,6 +31,10 @@ function mainEventController($timeout, $q, $scope, $state, eventsService, $mdDia
 			limit: 5,
 			order: 'full_name',
 			page: 1
+		};
+
+		vm.getMapsSrc = function () {
+			return $sce.trustAsResourceUrl('https://maps.google.com/maps?q='+vm.event.location+'&t=m&z=12&output=embed&iwloc=near');
 		};
 
 		vm.showRegistrationForm = function(ev) {
