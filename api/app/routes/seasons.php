5606b308a6bd2229b231da27ed18a3bd7c3e2ad1
Alex Resnick <adr8292@gmail.com>
2018-09-13 16:00:02 -0400

    Update seasons.php
    add mass notification function

--- api/app/routes/seasons.php
+++ api/app/routes/seasons.php
@@ -272,6 +272,18 @@ $app->group('/seasons', function () {
         $data['total'] = $totalNum;
         $data['maxPage'] = ceil($totalNum/$limit);
         $responseArr = array('status'=>true, 'msg'=>$formData['year'].' season created', 'data'=>$data);
+        //Send notifications
+        $msgData = array(
+          'slack' => array(
+            'title' => 'New Season Created',
+            'body' => 'The '.$newSeason->year.' FRC Season '.$newSeason->game_name.' has been created in the Team Portal.';
+          ),
+          'email' => array(
+            'subject' => 'New Season Created',
+            'content' =>  'The '.$newSeason->year.' FRC Season '.$newSeason->game_name.' has been created in the Team Portal.';
+          )
+        );
+        sendMassNotifications($type, $msgData);
       } else {
         $responseArr = array('status'=>false, 'msg'=>'Something went wrong');
       }
