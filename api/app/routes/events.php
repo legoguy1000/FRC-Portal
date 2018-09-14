ce61e2da7574d4fa11d2d56792e204ab4a6634ee
Alex Resnick <adr8292@gmail.com>
2018-09-14 06:06:08 -0400

    Update events.php
    add return badRequestResponse to routes

--- api/app/routes/events.php
+++ api/app/routes/events.php
@@ -199,9 +199,7 @@ $app->group('/events', function () {
         $event_id = $args['event_id'];
         $formData = $request->getParsedBody();
         if(!isset($formData['cars']) || !is_array($formData['cars']) || empty($formData['cars'])) {
-          $responseArr = array('status'=>false, 'msg'=>'Invalid request');
-          $response = $response->withJson($responseArr,400);
-          return $response;
+          return badRequestResponse($response);
         }
         $cars = FrcPortal\EventCar::where('event_id',$event_id)->get();
         foreach($cars as $car) {
@@ -254,19 +252,13 @@ $app->group('/events', function () {
         }
 
         if(!isset($formData['event_id']) || $formData['event_id'] == '') {
-          $responseArr['msg'] = 'Event ID cannot be blank';
-          $response = $response->withJson($responseArr,400);
-          return $response;
+          return badRequestResponse($response, $msg = 'Event ID cannot be blank');
         }
         if(!isset($formData['user_type']) || $formData['user_type'] == '') {
-          $responseArr['msg'] = 'User Type cannot be blank';
-          $response = $response->withJson($responseArr,400);
-          return $response;
+          return badRequestResponse($response, $msg = 'User Type cannot be blank');
         }
         if(!isset($formData['gender']) || ($formData['gender'] == '' && $formData['user_type'] != 'Mentor')) {
-          $responseArr['msg'] = 'Gender cannot be blank';
-          $response = $response->withJson($responseArr,400);
-          return $response;
+          return badRequestResponse($response, $msg = 'Gender cannot be blank');
         }
         $room = new FrcPortal\EventRoom();
         $room->event_id = $formData['event_id'];
@@ -292,9 +284,7 @@ $app->group('/events', function () {
         $event_id = $args['event_id'];
         $formData = $request->getParsedBody();
         if(!isset($formData['rooms']) || !is_array($formData['rooms']) || empty($formData['rooms'])) {
-          $responseArr = array('status'=>false, 'msg'=>'Invalid request');
-          $response = $response->withJson($responseArr,400);
-          return $response;
+          return badRequestResponse($response);
         }
         $rooms = FrcPortal\EventRoom::where('event_id',$event_id)->get();
         foreach($rooms as $room) {
@@ -561,14 +551,10 @@ $app->group('/events', function () {
 
       $event_id = $args['event_id'];
       if(!isset($formData['users']) || !is_array($formData['users']) || empty($formData['users'])) {
-        $responseArr = array('status'=>false, 'msg'=>'Please select at least 1 user');
-        $response = $response->withJson($responseArr,400);
-        return $response;
+        return badRequestResponse($response, $msg = 'Please select at least 1 user');
       }
-      if(!isset($formData['requirement']) || $formData['requirement'] == '' || !in_array($formData['requirement'],array('registration','permission_slip','payment','food'))) {
-        $responseArr = array('status'=>false, 'msg'=>'Invalid requirement');
-        $response = $response->withJson($responseArr,400);
-        return $response;
+      if(!isset($formData['requirement']) || $formData['requirement'] == '' || !in_array($formData['requirement'],array('registration','permission_slip','payment'))) {
+        return badRequestResponse($response, $msg = 'Invalid event requirement');
       }
       $event = FrcPortal\Event::find($event_id);
       $array = array();
@@ -608,9 +594,7 @@ $app->group('/events', function () {
       $userFullName = FrcPortal\Auth::user()->full_name;
 
       if(!is_bool($formData['registration'])) {
-        $responseArr = array('status'=>false, 'msg'=>'Invalid Request, no registration option.');
-        $response = $response->withJson($responseArr,400);
-        return $response;
+        return badRequestResponse($response, $msg = 'Invalid Request, no registration option.');
       }
 
       $user =  FrcPortal\User::find($user_id);
@@ -621,13 +605,9 @@ $app->group('/events', function () {
       $event = FrcPortal\Event::find($event_id);
       if($registrationBool) {
         if(time() > $event->date['start']['unix']) {
-          $responseArr = array('status'=>false, 'msg'=>'Registration is closed. Event has already started.');
-          $response = $response->withJson($responseArr,400);
-          return $response;
+          return badRequestResponse($response, $msg = 'Registration is closed. Event has already started.');
       	} elseif(($event->registration_deadline_date['unix'] != null && time() > $event->registration_deadline_date['unix']) && !FrcPortal\Auth::isAdmin()) {
-            $responseArr = array('status'=>false, 'msg'=>'Registration is closed. Registration deadline was '.date('F j, Y g:m A',$event->registration_deadline_unix).'.');
-            $response = $response->withJson($responseArr,400);
-            return $response;
+          return badRequestResponse($response, $msg = 'Registration is closed. Registration deadline was '.date('F j, Y g:m A',$event->registration_deadline_unix).'.'); 
       	}
         $reqUpdate = FrcPortal\EventRequirement::updateOrCreate(['event_id' => $event_id, 'user_id' => $user_id], ['registration' => true, 'comments' => $formData['comments']]);
         $ereq_id = $reqUpdate->ereq_id;
@@ -764,34 +744,21 @@ $app->group('/events', function () {
     }
 
     if(!isset($formData['name']) || $formData['name'] == '') {
-      $responseArr['msg'] = 'Name cannot be blank';
-      $response = $response->withJson($responseArr,400);
-      return $response;
+      return badRequestResponse($response, $msg = 'Name cannot be blank');
     }
     if(!isset($formData['type']) || $formData['type'] == '') {
-      $responseArr['msg'] = 'Event type cannot be blank';
-      $response = $response->withJson($responseArr,400);
-      return $response;
+      return badRequestResponse($response, $msg = 'Event type cannot be blank');
     }
     if(!isset($formData['event_start']) || $formData['event_start'] == '') {
-      $responseArr['msg'] = 'Start Date cannot be blank';
-      $response = $response->withJson($responseArr,400);
-      return $response;
-    }
+      return badRequestResponse($response, $msg = 'Start Date cannot be blank');
     if(!isset($formData['event_end']) || $formData['event_end'] == '') {
-      $responseArr['msg'] = 'End Date cannot be blank';
-      $response = $response->withJson($responseArr,400);
-      return $response;
+      return badRequestResponse($response, $msg = 'End Date cannot be blank');
     }
     if(strtotime($formData['event_start']) >= strtotime($formData['event_end'])) {
-      $$responseArr['msg'] = 'Start Date must be before End Date';
-      $response = $response->withJson($responseArr,400);
-      return $response;
+      return badRequestResponse($response, $msg = 'Start Date must be before End Date');
     }
     if(!isset($formData['google_cal_id']) || $formData['google_cal_id'] == '') {
-      $responseArr['msg'] = 'Invalid Google calendar ID';
-      $response = $response->withJson($responseArr,400);
-      return $response;
+      return badRequestResponse($response, $msg = 'Invalid Google calendar ID');
     }
     $events = FrcPortal\Event::where('google_cal_id', $formData['google_cal_id'])->count();
     if($events == 0) {
