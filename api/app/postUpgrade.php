1751fe36fb7790595b482328a02265b89c8c6d4b
Alex Resnick <adr8292@gmail.com>
2018-09-16 17:02:11 -0400

    update column

--- api/app/postUpgrade.php
+++ api/app/postUpgrade.php
@@ -103,9 +103,9 @@ if($version >= '2.10.0') {
     if(!Capsule::schema()->hasColumn('seasons','membership_form_sheet')) {
       try {
         Capsule::schema()->table('seasons', function ($table) {
-          $table->text('membership_form_sheet')->after('membership_form_map');
+          $table->string('membership_form_sheet')->nullable()->default('Form Responses 1');
         });
-      } catch (Exception $e) { 
+      } catch (Exception $e) {
         //Exception will be logged in Monolog
       }
     }
