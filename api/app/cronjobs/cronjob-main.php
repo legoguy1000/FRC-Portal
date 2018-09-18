00a094fc25802265a9059fb105e99886246d35ff
Alex Resnick <adr8292@gmail.com>
2018-09-18 19:16:50 -0400

    used = in boolean

--- api/app/cronjobs/cronjob-main.php
+++ api/app/cronjobs/cronjob-main.php
@@ -6,7 +6,7 @@ $changeUserStatus = getSettingsProp('enable_cronjob-changeUserStatus');
 if($changeUserStatus) {
   $users = array();
   $month = getSettingsProp('school_month_end');
-  if(!is_null($month) && $month != '' && date('F') == $month && date('Y-m-t') = date('Y-m-d')) {
+  if(!is_null($month) && $month != '' && date('F') == $month && date('Y-m-t') == date('Y-m-d')) {
   	$users = FrcPortal\User::where('user_type','Student')->whereNotNull('grad_year')->where('grad_year',date('Y'))->update(['status' => false, 'admin' => false, 'former_student' => true]);
   }
 }
