10ea1cf30004f44b0f2d377bdafc6bee054c4c41
Alex Resnick <adr8292@gmail.com>
2018-09-21 22:11:57 -0400

    update report endpoints to use standardized functions

--- api/app/functions/report_functions.php
+++ api/app/functions/report_functions.php
@@ -38,4 +38,35 @@ function checkReportInputs($request, $response, $type = 'range') {
     return true;
 }
 
+function initializeMultiYearData($start = 0, $end = 0, $series = array()) {
+	$years = array();
+	for($i = $start; $i <= $end; $i++) {
+		$years[] = (integer) $i;
+	}
+	$data = array();
+	foreach($series as $se) {
+		$data[$se] = array_fill_keys($years,0);
+	}
+	return array(
+		'years' => $years,
+		'data' => $data,
+	);
+}
+
+function multiYearReportData($data = array(), $series = array(), $years = array()) {
+	$newData = array();
+	foreach($series as $se) {
+		$newData[$se] = array_values($data[$se]);
+	}
+
+	$allData = array(
+		'labels' => $years,
+		'series' => $series,
+		'data' => array_values($newData),
+		//'csvData' => metricsCreateCsvData($data, $years, $series)
+	);
+
+	return $allData;
+}
+
 ?>
