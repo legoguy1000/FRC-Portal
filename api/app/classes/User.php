31b2cad3b95d3f059757edd3cfb30a9725802215
Alex Resnick <adr8292@gmail.com>
2018-10-04 20:29:52 -0400

    add notification

--- api/app/classes/User.php
+++ api/app/classes/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Capsule\Manager as DB;
 use \DateTime;
 
 class User extends Eloquent {
+  use Notifiable;
   //table name
   protected $table = 'users';
   //Use Custom Primary Key
