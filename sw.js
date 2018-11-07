12635440bd7a76ee8c4ca6ab87c1740f3b59ea3d
Alex Resnick <adr8292@gmail.com>
2018-11-06 21:38:03 -0500

    disable cache

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@ self.addEventListener('fetch', function(event) {
       .then(function(response) {
         // Cache hit - return response
         if (response) {
-          return response;
+          //return response;
         }
         return fetch(event.request);
       }
