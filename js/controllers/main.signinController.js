24f0cc2baa91c82e27b65f61ef078dfde91e96a4
Alex Resnick <adr8292@gmail.com>
2018-11-06 21:46:49 -0500

    disable event source atm

--- js/controllers/main.signinController.js
+++ js/controllers/main.signinController.js
@@ -79,7 +79,7 @@ function mainSigninController($rootScope, $timeout, $q, $auth, $scope, signinSer
 			if(response.status && response.signin_token != undefined) {
 				signinService.saveToken(response.signin_token);
 				vm.qrCodeUrl = vm.genQrCodeUrl();
-				startEventSource();
+				//startEventSource();
 			}
 			vm.signInAuthed = signinService.isAuthed();
 		});
