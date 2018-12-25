a3d6f08ecb0d5c949043d424ee6f1be47f58ab70
Alex Resnick <adr8292@gmail.com>
2018-12-25 17:02:10 -0600

    default user_id is null

--- api/app/database/Logs.php
+++ api/app/database/Logs.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 Capsule::schema()->create('logs', function ($table) {
   $table->char('log_id',13)->primary();
   $table->string('level',13);
-  $table->char('user_id',13)->index();
+  $table->char('user_id',13)->index()->nullable()->default(null);;
   $table->text('message');
   $table->timestamps();
 });
