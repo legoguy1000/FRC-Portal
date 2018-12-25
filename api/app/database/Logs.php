7bd9f927a4acb8590c74734f866e5eb3c6cfa3b8
Alex Resnick <adr8292@gmail.com>
2018-12-25 17:02:58 -0600

    set string no limit

--- api/app/database/Logs.php
+++ api/app/database/Logs.php
@@ -5,7 +5,7 @@ require_once(__DIR__.'/../includes.php');
 use Illuminate\Database\Capsule\Manager as Capsule;
 Capsule::schema()->create('logs', function ($table) {
   $table->char('log_id',13)->primary();
-  $table->string('level',13);
+  $table->string('level');
   $table->char('user_id',13)->index()->nullable()->default(null);;
   $table->text('message');
   $table->timestamps();
