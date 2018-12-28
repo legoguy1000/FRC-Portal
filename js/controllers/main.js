515a40131902c7ee494c7344455441ba0e185ba9
Alex Resnick <adr8292@gmail.com>
2018-12-28 14:52:43 -0600

    update new instascan.js

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -42,7 +42,7 @@ function mainController($rootScope, configItems, $auth, navService, $mdSidenav, 
 	$ocLazyLoad.load('js/controllers/serviceAccountModalController.js');
 	$ocLazyLoad.load('js/controllers/googleFormMapModalController.js');
 	$ocLazyLoad.load('js/controllers/signInModalController.js');
-  $ocLazyLoad.load('js/directives/instascan.min.js');
+  $ocLazyLoad.load('https://rawgit.com/schmich/instascan-builds/master/instascan.min.js');
 	$ocLazyLoad.load('js/controllers/newSchoolModalController.js');
 
 	navService
