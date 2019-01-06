f125e59c1ac7099ddffdae8db04c5e9e26b6464b
Alex Resnick <adr8292@gmail.com>
2019-01-05 20:04:26 -0600

    didn't include api/

--- js/services/loginServices.js
+++ js/services/loginServices.js
@@ -9,37 +9,37 @@ angular.module('FrcPortal')
 					msg: 'Invalid login attempt'
 				};
 			}
-			return $http.post('auth/'+pro,formData)
+			return $http.post('api/auth/'+pro,formData)
 			.then(function(response) {
 				return response.data;
 			});
 		},
 		google: function (formData) {
-			return $http.post('auth/google',formData)
+			return $http.post('api/auth/google',formData)
 			.then(function(response) {
 				return response.data;
 			});
 		},
 		facebook: function (formData) {
-			return $http.post('auth/facebook',formData)
+			return $http.post('api/auth/facebook',formData)
 			.then(function(response) {
 				return response.data;
 			});
 		},
 		microsoft: function (formData) {
-			return $http.post('auth/microsoft',formData)
+			return $http.post('api/auth/microsoft',formData)
 			.then(function(response) {
 				return response.data;
 			});
 		},
 		amazon: function (formData) {
-			return $http.post('auth/amazon',formData)
+			return $http.post('api/auth/amazon',formData)
 			.then(function(response) {
 				return response.data;
 			});
 		},
 		github: function (formData) {
-			return $http.post('auth/github',formData)
+			return $http.post('api/auth/github',formData)
 			.then(function(response) {
 				return response.data;
 			});
