b89b9fb531d5b36e8e77cb8c549f284315342347
Alex Resnick <adr8292@gmail.com>
2019-01-21 15:57:42 -0600

    edits events instead of seasons

--- api/app/routes/events.php
+++ api/app/routes/events.php
@@ -819,7 +819,6 @@ $app->group('/events', function () {
           $eventCarUpdate = FrcPortal\EventCar::where('event_id',$event_id)->where('user_id',$user)->delete();
         } else {
           $reqArr->$req = $new;
-          $reqArr->{$req.'_date'} = date('Y-m-d H:i:s');
           $reqArr->save();
         }
       }
