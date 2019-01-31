26440df3d48ac1a829f684ddeb1630bba0db6b21
Alex Resnick <adr8292@gmail.com>
2019-01-30 19:15:32 -0500

    add rootScope

--- js/controllers/signInModalController.js
+++ js/controllers/signInModalController.js
@@ -1,8 +1,8 @@
 angular.module('FrcPortal')
-.controller('signInModalController', ['$log','$element','$mdDialog', '$scope', 'usersService','$mdToast','userInfo','signinService','$interval','$document','$timeout','$document',
+.controller('signInModalController', ['$rootScope','$log','$element','$mdDialog', '$scope', 'usersService','$mdToast','userInfo','signinService','$interval','$document','$timeout','$document',
 	signInModalController
 ]);
-function signInModalController($log,$element,$mdDialog,$scope,usersService,$mdToast,userInfo,signinService,$interval,$document,$timeout,$document) {
+function signInModalController($rootScope,$log,$element,$mdDialog,$scope,usersService,$mdToast,userInfo,signinService,$interval,$document,$timeout,$document) {
 	var vm = this;
 
 	vm.cancel = function() {
