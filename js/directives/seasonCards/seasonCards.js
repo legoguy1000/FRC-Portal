3f6fb5c7667de42b497753196b0f1b0a8c3d825a
Alex Resnick <adr8292@gmail.com>
2019-06-29 10:45:42 -0400

    figured it out, was placing before app.js

--- js/directives/seasonCards/seasonCards.js
+++ js/directives/seasonCards/seasonCards.js
@@ -1,16 +1,13 @@
 angular.module('FrcPortal')
 .directive('seasonCards', function() {
 	return {
-		require: 'ngModel',
 		restrict: 'E',
 		transclude: true,
 		scope: { seasons:'=' },
 		templateUrl: 'js/directives/seasonCards/seasonCards.html',
     controller: function($scope) {
-      var dir = this;
-      dir.limit = 3;
+      $scope.limit = 3;
     },
-    controllerAs: 'dir',
     link: function ($scope, element, attrs) { } //DOM manipulation
 	};
 });
