210a25886a92e1abad585b4959efda2bdc4ef1fc
Alex Resnick <adr8292@gmail.com>
2019-06-29 10:16:43 -0400

    maybe this will work

--- js/directives/seasonCards/seasonCards.js
+++ js/directives/seasonCards/seasonCards.js
@@ -2,12 +2,12 @@ angular.module('FrcPortal')
 .directive('seasonCards', function() {
 	return {
 		restrict: 'E',
-		replace: true,
 		transclude: true,
 		scope: { seasons:'@' },
 		templateUrl: 'seasonCards.html',
     controller: function($scope) {
       $scope.limit = 3;
     }
+    link: function ($scope, element, attrs) { } //DOM manipulation
 	};
 });
