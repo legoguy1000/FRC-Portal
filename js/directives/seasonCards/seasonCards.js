830bd2528e67cedf1a8ae12778a6b6d7fb524f4b
Alex Resnick <adr8292@gmail.com>
2019-06-29 10:09:07 -0400

    trying to fix directive

--- js/directives/seasonCards/seasonCards.js
+++ js/directives/seasonCards/seasonCards.js
@@ -8,13 +8,6 @@ angular.module('FrcPortal')
 		templateUrl: 'seasonCards.html',
     controller: function($scope) {
       $scope.limit = 3;
-    },
-		compile: function(element, attrs, linker) {
-			return function(scope, element) {
-			  linker(scope, function(clone) {
-				element.append(clone);
-			  });
-			};
-		}
+    }
 	};
 });
