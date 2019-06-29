0588533feb3d1a30174b096c0e437385eaa3bcee
Alex Resnick <adr8292@gmail.com>
2019-06-29 10:01:25 -0400

    missing ,

--- js/directives/seasonCards/seasonCards.js
+++ js/directives/seasonCards/seasonCards.js
@@ -8,7 +8,7 @@ angular.module('FrcPortal')
 		templateUrl: 'seasonCards.html',
     controller: function($scope) {
       $scope.limit = 3;
-    }
+    },
 		compile: function(element, attrs, linker) {
 			return function(scope, element) {
 			  linker(scope, function(clone) {
