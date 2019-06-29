4294b5cc24634cd8da24443a56ce720f07260322
Alex Resnick <adr8292@gmail.com>
2019-06-29 09:59:40 -0400

    create directive

---
+++ js/directives/seasonCards/seasonCards.js
@@ -0,0 +1,20 @@
+angular.module('FrcPortal')
+.directive('seasonCards', function() {
+	return {
+		restrict: 'E',
+		replace: true,
+		transclude: true,
+		scope: { seasons:'@' },
+		templateUrl: 'seasonCards.html',
+    controller: function($scope) {
+      $scope.limit = 3;
+    }
+		compile: function(element, attrs, linker) {
+			return function(scope, element) {
+			  linker(scope, function(clone) {
+				element.append(clone);
+			  });
+			};
+		}
+	};
+});
