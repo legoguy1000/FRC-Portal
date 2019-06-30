729235e0ed329b6b9917a2e07998d83bd57f4308
Alex Resnick <adr8292@gmail.com>
2019-06-30 11:50:33 -0400

    use functions to log

--- js/directives/seasonCards/seasonCards.js
+++ js/directives/seasonCards/seasonCards.js
@@ -7,6 +7,15 @@ angular.module('FrcPortal')
 		templateUrl: 'js/directives/seasonCards/seasonCards.html',
     controller: function($scope) {
       $scope.limit = $scope.max;
+
+			$scope.increase = function() {
+				$scope.limit = $scope.limit+1;
+				console.log($scope.limit);
+			}
+			$scope.decrease = function() {
+				$scope.limit = $scope.limit-1;
+				console.log($scope.limit);
+			}
     },
     link: function ($scope, element, attrs) { } //DOM manipulation
 	};
