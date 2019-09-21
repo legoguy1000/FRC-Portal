4c6747eb873b9ea7d7e79500032fa601cf4b5634
Alex Resnick <adr8292@gmail.com>
2019-09-21 12:16:38 -0500

    update defaults

--- api/app/database/User.php
+++ api/app/database/User.php
@@ -7,7 +7,7 @@ Capsule::schema()->create('users', function ($table) {
   $table->char('user_id',13)->primary();
   $table->string('fname')->default('');
   $table->string('lname')->default('');
-  $table->string('email')->unique();
+  $table->string('email')->default('')->unique();
   $table->char('password',128)->nullable()->default(null);
   $table->char('school_id',13)->nullable()->default(null)->index();
   $table->string('user_type')->default('');
@@ -16,7 +16,7 @@ Capsule::schema()->create('users', function ($table) {
   $table->string('team_email')->nullable()->default(null);
   $table->char('phone',10)->nullable()->default(null);
   $table->string('gender')->default('');
-  $table->string('profile_image',500);
+  $table->string('profile_image',500)->default('');
   $table->string('slack_id')->default('');
   $table->char('signin_pin',64)->default('');
   $table->boolean('admin')->default(0);
