2e8bb8a688412f8d8735c56c10e5306b29080e81
Alex Resnick <adr8292@gmail.com>
2019-09-27 21:10:38 -0500

    add loading

--- js/controllers/oAuthCredentialModalController.js
+++ js/controllers/oAuthCredentialModalController.js
@@ -10,8 +10,10 @@ function oAuthCredentialModalController($log,$mdDialog,$scope,settingsService,$m
 	}
 
 	vm.data = {};
+	vm.loading = false;
 
 	vm.updateCredentials = function() {
+		vm.loading = true;
 		var data = {
 			'client_id': vm.data.client_id,
 			'client_secret': vm.data.client_secret,
@@ -27,10 +29,12 @@ function oAuthCredentialModalController($log,$mdDialog,$scope,settingsService,$m
 	        .position('top right')
 	        .hideDelay(3000)
 	    );
+			vm.loading = false;
 		});
 	}
 
 	vm.getOAuthCredentialsByProvider = function() {
+		vm.loading = true;
 		settingsService.getOAuthCredentialsByProvider(provider).then(function(response) {
 			if(response.status) {
 				vm.data = response.data
@@ -42,6 +46,7 @@ function oAuthCredentialModalController($log,$mdDialog,$scope,settingsService,$m
 						.hideDelay(3000)
 				);
 			}
+			vm.loading = false;
 		});
 	}
 	vm.getOAuthCredentialsByProvider();
