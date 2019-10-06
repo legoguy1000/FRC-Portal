3ef9851bd4d25f76284aa757b1856c3dd229f0db
Alex Resnick <adr8292@gmail.com>
2019-10-06 10:27:42 -0500

    update sse

--- api/sse.php
+++ api/sse.php
@@ -10,17 +10,35 @@ header("Cache-Control: no-cache");
 header('Connection: keep-alive');
 header('X-Accel-Buffering: no');//Nginx: unbuffered responses suitable for Comet and HTTP streaming applications
 
+$lastEventTimeStamp = floatval(isset($_SERVER["HTTP_LAST_EVENT_ID"]) ? $_SERVER["HTTP_LAST_EVENT_ID"] : 0);
+if ($lastEventTimeStamp == 0) {
+  $lastEventTimeStamp = floatval(isset($_GET["lastEventId"]) ? $_GET["lastEventId"] : 0);
+}
+
+echo ":" . str_repeat(" ", 2048) . "\n"; // 2 kB padding for IE
+echo "retry: 2000\n";
+
 //5a57eeed0b3ab
 // start stream
 while(true) {
-  $lastEventTimeStamp = array("time"=>date('Y-m-d H:i:s'));
-  echo "id: " . $lastEventTimeStamp['time'] . "\n";
-  echo "data: ".json_encode($lastEventTimeStamp)." \n\n";
-  echo PHP_EOL;
-  if (ob_get_contents()) {
-    ob_end_flush();
+  if(connection_aborted()) {
+    exit();
+  } else {
+    // here you will want to get the latest event id you have created on the server, but for now we will increment and force an update
+    $hours = FrcPortal\MeetingHour::where('updated_at','>',$lastEventTimeStamp)->get();
+    if(count($hours) > 0) {
+      sleep(2);
+      $users = getSignInList(date('Y'));
+      $lastEventTimeStamp = date('Y-m-d H:i:s');
+      echo "id: " . $lastEventTimeStamp . "\n";
+      echo "data: ".json_encode($users)." \n\n";
+      echo PHP_EOL;
+      if (ob_get_contents()) {
+        ob_end_flush();
+      }
+      flush();
+    }
   }
-  flush();
   // 2 second sleep then carry on
   sleep(2);
 }
