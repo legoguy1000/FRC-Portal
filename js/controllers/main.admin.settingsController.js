95513305bf07b4fa062d3a921a78822fbe4e0fb8
Alex Resnick <adr8292@gmail.com>
2019-10-11 18:44:27 -0500

    show current version if no update avaiable

--- js/controllers/main.admin.settingsController.js
+++ js/controllers/main.admin.settingsController.js
@@ -154,6 +154,9 @@ function mainAdminSettingsController($rootScope, $state, $timeout, $q, $scope, s
 		settingsService.checkUpdates().then(function(response){
 			var latest_release = response.data.latest_release;
 			var latest_version = response.data.latest_version;
+			if(latest_release==null && response.data.update_available==false) {
+				latest_release = vm.update.current_version;
+			}
 			vm.update.latest_version = latest_release+'-'+latest_version.substring(0, 6);
 		});
 	}
