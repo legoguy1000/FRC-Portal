8cd16af72285be6a3d1c3793714873da6be94f9e
Alex Resnick <adr8292@gmail.com>
2019-10-13 12:56:04 -0500

    log trans

--- js/app.js
+++ js/app.js
@@ -692,6 +692,7 @@ angular.module('FrcPortal', [
 	// track pageview on state change
 	$transitions.onStart({}, function(transition) {
 		$rootScope.pageRefresh = false;
+		console.log(transition.from());
 		if(transition.from() == null) {
 			$rootScope.pageRefresh = true;
 		}
