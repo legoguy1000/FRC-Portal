1b83b1a96f8e04dc4d9b2d9075a33bcc86acae72
Alex Resnick <adr8292@gmail.com>
2019-10-18 18:33:06 -1000

    test

--- api/app/classes/AnnualRequirement.php
+++ api/app/classes/AnnualRequirement.php
@@ -91,8 +91,7 @@ class AnnualRequirement extends Eloquent {
       $seasonInfo = Season::find($this->season_id);
       $no_bagday = $seasonInfo->no_bagday;
       return $this->hasOne('FrcPortal\MeetingHour', 'user_id', 'user_id')
-                  ->selectRaw('meeting_hours.user_id, year(meeting_hours.time_in), SUM(time_to_sec(IFNULL(timediff(meeting_hours.time_out, meeting_hours.time_in),0)) / 3600) AS build_season_hours')
-                  ->groupBy('meeting_hours.user_id');
+                  ->select(DB::raw('SUM(time_to_sec(IFNULL(timediff(meeting_hours.time_out, meeting_hours.time_in),0)) / 3600) as build_season_hours'))->groupBy('meeting_hours.user_id')->first()
   //  }
   }
 
