d56702d7cf5acb1940e2929e10473dff99f2972d
Alex Resnick <adr8292@gmail.com>
2019-11-02 23:26:03 -0500

    undo

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -185,28 +185,7 @@ function mainController($rootScope, configItems, $auth, navService, $mdSidenav, 
 	    	window.PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable().then(response => {
 	      if (response == true) {
 					$mdDialog.show(confirm).then(function() {
-						var challenge = new Uint8Array(32);
-						window.crypto.getRandomValues(challenge);
-						var userID = 'Kosv9fPtkDoh4Oz7Yq/pVgWHS8HhdlCto5cR0aBoVMw='
-						var id = Uint8Array.from(window.atob(userID), c=>c.charCodeAt(0))
-						var publicKey = {
-						    'challenge': challenge,
-						    'rp': {
-						        'name': 'Example Inc.'
-						    },
-						    'user': {
-						        'id': id,
-						        'name': 'alice@example.com',
-						        'displayName': 'Alice Liddell'
-						    },
-						    'pubKeyCredParams': [
-						        { 'type': 'public-key', 'alg': -7  },
-						        { 'type': 'public-key', 'alg': -257 }
-						    ]
-						}
-						navigator.credentials.create({ 'publicKey': publicKey }).then(newCredentialInfo => {
-						        console.log('SUCCESS', newCredentialInfo)
-						    });
+						console.log('i want to register');
 					}, function() {
 						console.log('i dont want to register');
 					});
