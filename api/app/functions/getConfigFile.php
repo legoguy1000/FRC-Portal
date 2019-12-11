b6dc821de5aaba98e593204c0da67a3a5a92712e
Alex Resnick <adr8292@gmail.com>
2019-12-10 21:19:13 -0500

    add section to settings get

--- api/app/functions/getConfigFile.php
+++ api/app/functions/getConfigFile.php
@@ -9,9 +9,13 @@ function getIniProp($prop) {
 	return $value;
 }
 
-function getSettingsProp($prop) {
+function getSettingsProp($prop, $section = null) {
 	$value = null;
-	$setting = FrcPortal\Setting::where('setting',$prop)->first();
+	$setting = FrcPortal\Setting::where('setting',$prop);
+	if($section != null) {
+		$setting = $setting->where('section',$section);
+	}
+	$setting = $setting->first();
 	//$ini = parse_ini_file(__DIR__.'/../secured/config.ini');
 	if(!is_null($setting) && isset($setting->value)) {
 		$value = $setting->value;
