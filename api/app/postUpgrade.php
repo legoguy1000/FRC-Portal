c81ecf9d89ddb9bb23e34e1191dbb02018e5ac7e
Alex Resnick <adr8292@gmail.com>
2019-12-11 12:22:14 -0500

    same change for hotel_info

--- api/app/postUpgrade.php
+++ api/app/postUpgrade.php
@@ -296,7 +296,7 @@ if($version >= '2.16.0') {
     if(Capsule::schema()->hasColumn('events','hotel_info')) {
       try {
         Capsule::schema()->table('events', function ($table, $as = null, $connection = null) {
-          $table->string('hotel_info')->default('')->change();
+        $table->text('hotel_info')->nullable()->default(null)->change();
         });
       } catch (Exception $e) {
         //Exception will be logged in Monolog
