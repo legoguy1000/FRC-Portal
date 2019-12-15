415b7445b93d0de711d15385f4f63ee2a46a40d3
Alex Resnick <adr8292@gmail.com>
2019-12-15 15:13:34 -0600

    remove unique

--- api/app/database/UserCredential.php
+++ api/app/database/UserCredential.php
@@ -5,7 +5,7 @@ require_once(__DIR__.'/../includes.php');
 use Illuminate\Database\Capsule\Manager as Capsule;
 Capsule::schema()->create('user_credentials', function ($table) {
      $table->char('cred_id',13)->primary();
-     $table->text('credential_id')->unique();
+     $table->text('credential_id');
      $table->text('public_key');
      $table->text('user_handle');
      $table->char('user_id',13)->nullable()->default(null);
