2d2af3bcdb908baeec82d69b87fb4d95215304d7
Alex Resnick <adr8292@gmail.com>
2019-12-21 21:13:53 -0600

    remove configfile function file

--- api/app/classes/Traits/Encryption.php
+++ api/app/classes/Traits/Encryption.php
@@ -1,5 +1,7 @@
 <?php
 
+use FrcPortal\Utilities\IniConfig;
+
 namespace FrcPortal\Traits;
 
 /**
@@ -11,7 +13,7 @@ trait Encryption {
 
   public function encrypt($decrypted) {
   	$nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
-  	$key = hex2bin(getIniProp('encryption_key'));
+  	$key = hex2bin(IniConfig::iniDataProperty('encryption_key'));
   	$ciphertext = sodium_crypto_secretbox($decrypted, $nonce, $key);
   	$encrypted = base64_encode($nonce.$ciphertext);
   	return $encrypted;
@@ -21,7 +23,7 @@ trait Encryption {
   	$decoded = base64_decode($encrypted);
   	$nonce = mb_substr($decoded, 0, SODIUM_CRYPTO_SECRETBOX_NONCEBYTES, '8bit');
   	$ciphertext = mb_substr($decoded, SODIUM_CRYPTO_SECRETBOX_NONCEBYTES, null, '8bit');
-  	$key = hex2bin(getIniProp('encryption_key'));
+  	$key = hex2bin(IniConfig::iniDataProperty('encryption_key'));
   	$decrypted = sodium_crypto_secretbox_open($ciphertext, $nonce, $key);
   	return $decrypted;
   }
--- api/app/functions/general_functions.php
+++ api/app/functions/general_functions.php
@@ -28,6 +28,19 @@ function getRealIpAddr() {
     return $ip;
 }
 
+function getSettingsProp($prop, $section = null) {
+	$value = null;
+	$setting = FrcPortal\Setting::where('setting',$prop);
+	if($section != null) {
+		$setting = $setting->where('section',$section);
+	}
+	$setting = $setting->first();
+	//$ini = parse_ini_file(__DIR__.'/../secured/config.ini');
+	if(!is_null($setting) && isset($setting->value)) {
+		$value = $setting->value;
+	}
+	return $value;
+}
 /*
 function insertLogs($userId, $type, $status, $msg) {
 	$db = db_connect();
