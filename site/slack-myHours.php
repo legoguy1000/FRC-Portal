2fc505d11c6cd584057bb2d3e8131f4c75c13896
Alex Resnick <adr8292@gmail.com>
2019-12-21 22:05:09 -0600

    remove old code

--- site/slack-myHours.php
+++ site/slack-myHours.php
@@ -5,17 +5,17 @@ $token = $_POST['token'];
 $user_id = $_POST['user_id'];
 $user_name = $_POST['user_name'];
 
-$where = ' WHERE slack_id = '.db_quote($user_id);
-$query = userQuery($sel='',$joins='', $where, $order = '');
-//die($query);
-$result = db_select_single($query);
-if(!is_null($result)) {
-	//die('yes');
-	$userInfo = userSeasonInfo($user_id = $result['user_id'], $year = date('Y'));
-	die($userInfo[0]['total']);
-}  else {
-	die('I don\'t know who you are.  Please use the /registerPortal command to register Slack user ID.');
-}
+// $where = ' WHERE slack_id = '.db_quote($user_id);
+// $query = userQuery($sel='',$joins='', $where, $order = '');
+// //die($query);
+// $result = db_select_single($query);
+// if(!is_null($result)) {
+// 	//die('yes');
+// 	$userInfo = userSeasonInfo($user_id = $result['user_id'], $year = date('Y'));
+// 	die($userInfo[0]['total']);
+// }  else {
+// 	die('I don\'t know who you are.  Please use the /registerPortal command to register Slack user ID.');
+// }
 
 
 
