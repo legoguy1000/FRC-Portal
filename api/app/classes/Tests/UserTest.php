a3abe837ed7fbe4f19da5ee2e10f0bd726aab48e
Alex Resnick <adr8292@gmail.com>
2019-12-29 12:36:00 -0600

    use setUpBeforeClass

--- api/app/classes/Tests/UserTest.php
+++ api/app/classes/Tests/UserTest.php
@@ -10,10 +10,10 @@ use FrcPortal\Utilities\IniConfig;
 class UserTest extends TestCase {
 
   protected $app;
-  protected $jwt;
-  protected $user;
+  protected static $jwt;
+  protected static $user;
 
-  public function setUp(): void {
+  public static function setUpBeforeClass() {
     $this->app = (new FrcPortal\App())->get();
     $user = new FrcPortal\User();
     $user->email = 'abcd@example.org';
@@ -26,24 +26,46 @@ class UserTest extends TestCase {
     $user->status = true;
     //$user->getGetSlackIdByEmail();
     $user->save();
-    $this->user = $user;
-    $this->jwt = $user->generateUserJWT();
+    self::$user = $user;
+    self::$jwt = $user->generateUserJWT();
     //$this->app->config('debug', true);
   }
 
-  public function tearDown(): void {
-    unset($this->app);
-    FrcPortal\User::destroy($this->user->user_id);
+  public static function tearDownAfterClass() {
+      FrcPortal\User::destroy(self::$user->user_id);
   }
+
+  // public function setUp(): void {
+  //   $this->app = (new FrcPortal\App())->get();
+  //   $user = new FrcPortal\User();
+  //   $user->email = 'abcd@example.org';
+  //   $user->fname = 'John';
+  //   $user->lname = 'Doe';
+  //   $user->getGenderByFirstName();
+  //   $user->user_type = 'Mentor';
+  //   $user->phone = '1234567890';
+  //   $user->admin = true;
+  //   $user->status = true;
+  //   //$user->getGetSlackIdByEmail();
+  //   $user->save();
+  //   $this->user = $user;
+  //   $this->jwt = $user->generateUserJWT();
+  //   //$this->app->config('debug', true);
+  // }
+  //
+  // public function tearDown(): void {
+  //   unset($this->app);
+  //   FrcPortal\User::destroy($this->user->user_id);
+  // }
 
 
   public function testUserJwt() {
     $authToken = JWT::decode(
-        $this->jwt,
+        self::$jwt,
         getSettingsProp('jwt_key') ? getSettingsProp('jwt_key') : IniConfig::iniDataProperty('db_pass'),
         array("HS256", "HS512", "HS384")
     );
-    $this->assertSame($authToken->data->user_id , $this->user->user_id);
+    $this->assertSame($authToken->data->user_id , self::$user->user_id);
   }
 
   public function testGetUsers() {
@@ -53,7 +75,7 @@ class UserTest extends TestCase {
     ]);
     $request = Request::createFromEnvironment($env);
     $request = $request->withHeader('Content-Type', 'application/json');
-    $request = $request->withHeader('Authorization', 'Bearer '.$this->jwt);
+    $request = $request->withHeader('Authorization', 'Bearer '.self::$jwt);
     $this->app->getContainer()['request'] = $request;
     $response = $this->app->run(true);
     $this->assertSame($response->getStatusCode(), 200);
@@ -64,23 +86,23 @@ class UserTest extends TestCase {
     $this->assertGreaterThanOrEqual(1 , count($body->data));
   }
 
-  // public function testGetUser() {
-  //   $env = Environment::mock([
-  //     'REQUEST_METHOD' => 'GET',
-  //     'REQUEST_URI'    => '/users/'.$this->user->user_id,
-  //   ]);
-  //   $request = Request::createFromEnvironment($env);
-  //   $request = $request->withHeader('Content-Type', 'application/json');
-  //   $request = $request->withHeader('Authorization', 'Bearer '.$this->jwt);
-  //   $this->app->getContainer()['request'] = $request;
-  //   $response = $this->app->run(true);
-  //   $this->assertSame($response->getStatusCode(), 200);
-  //   $body = json_decode((string) $response->getBody());
-  //   $this->assertSame('application/json', $response->getHeaderLine('Content-Type'));
-  //   $this->assertTrue($body->status);
-  //   $this->assertObjectHasAttribute('data' , $body);
-  //   $this->assertSame($body->data->user_id , $this->user->user_id);
-  // }
+  public function testGetUser() {
+    $env = Environment::mock([
+      'REQUEST_METHOD' => 'GET',
+      'REQUEST_URI'    => '/users/'.self::$user->user_id,
+    ]);
+    $request = Request::createFromEnvironment($env);
+    $request = $request->withHeader('Content-Type', 'application/json');
+    $request = $request->withHeader('Authorization', 'Bearer '.self::$jwt);
+    $this->app->getContainer()['request'] = $request;
+    $response = $this->app->run(true);
+    $this->assertSame($response->getStatusCode(), 200);
+    $body = json_decode((string) $response->getBody());
+    $this->assertSame('application/json', $response->getHeaderLine('Content-Type'));
+    $this->assertTrue($body->status);
+    $this->assertObjectHasAttribute('data' , $body);
+    $this->assertSame($body->data->user_id , self::$user->user_id);
+  }
 }
 
 ?>
